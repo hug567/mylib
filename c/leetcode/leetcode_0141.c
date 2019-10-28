c5cf0f4b3937d072941af0d87ad46f5e9c82a1a6
Huang Xing <huangxing567@163.com>
2019-10-28 07:59:20 -0700

    Complete leetcode 141
    Signed-off-by: Huang Xing <huangxing567@163.com>

--- c/leetcode/leetcode_0141.c
+++ c/leetcode/leetcode_0141.c
@@ -1,6 +1,7 @@
 /* leetcode 141: 环形链表(简单) */
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 struct ListNode {
 	int val;
@@ -9,18 +10,31 @@ struct ListNode {
 
 bool hasCycle(struct ListNode *head)
 {
-	return 0;
+	struct ListNode *slow = head, *fast = head;
+
+	while (fast != NULL && fast->next != NULL) {
+		/* 快慢指针 */
+		slow = slow->next;
+		fast = fast->next->next;
+		if (fast == NULL || fast->next == NULL) {
+			return false;
+		} if (slow == fast) {
+			return true;
+		}
+	}
+	return false;
 }
 
 int main()
 {
 	int i;
-	int val[] = {3, 2, 0, 4};
+	int val[] = {3, 2, 0, -4};
 	struct ListNode *head = (struct ListNode *)malloc(sizeof(struct ListNode));
 	struct ListNode *next = NULL, *tmp = NULL;
 	head->val = val[0];
 	head->next = NULL;
 
+	/* 构造环形链表 */
 	tmp = head;
 	for (i = 1; i < 4; i++) {
 		next = (struct ListNode *)malloc(sizeof(struct ListNode));
@@ -32,8 +46,8 @@ int main()
 	tmp->next = head->next;
 
 	tmp = head;
-	printf("listA : ");
-	while (tmp != NULL) {
+	printf("list : ");
+	for (i = 0; i < 4; i++) {
 		printf("%d ", tmp->val);
 		tmp = tmp->next;
 	}
