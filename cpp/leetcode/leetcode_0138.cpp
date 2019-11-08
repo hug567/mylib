18f5afc59fe070d2f010cbbe022fe807f8bb989a
Huang Xing <huangxing567@163.com>
2019-11-08 05:35:33 -0800

    Add cpp
    Signed-off-by: Huang Xing <huangxing567@163.com>

---
+++ cpp/leetcode/leetcode_0138.cpp
@@ -0,0 +1,32 @@
+/* leetcode 138: 复制带随机指针的链表(中等) */
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+	int val;
+	Node* next;
+	Node* random;
+
+	Node() {}
+
+	Node(int _val, Node* _next, Node* _random) {
+		val = _val;
+		next = _next;
+		random = _random;
+	}
+};
+
+
+class Solution {
+public:
+	Node* copyRandomList(Node* head) {
+
+	}
+};
+
+int main()
+{
+	cout << "hello first cpp" << endl;
+	return 0;
+}
---
+++ cpp/structure/list.cpp
@@ -0,0 +1,9 @@
+/* 单链表 */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "hello first cpp" << endl;
+	return 0;
+}
