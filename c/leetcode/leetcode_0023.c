61f7c35400875543edd623bdb0900e81272a3ccb
Huang Xing <huangxing567@163.com>
2019-11-17 07:30:56 -0800

    Rename vimscript
    Signed-off-by: Huang Xing <huangxing567@163.com>

--- c/leetcode/leetcode_0023.c
+++ c/leetcode/leetcode_0023.c
@@ -10,16 +10,31 @@ struct ListNode {
 
 struct ListNode* mergeKLists(struct ListNode** lists, int listsSize)
 {
+	int i = 0;
 	struct ListNode *head = NULL;
+
+//	for (i = 0; i < listsSize; i++) {
+//		head = lists[i];
+//		printf("list%d: ", i);
+//		while (head != NULL) {
+//			printf("%d ", head->val);
+//			head = head->next;
+//		}
+//		printf("\n");
+//	}
+
+
+
 	return head;
 }
 
 int main()
 {
 	int i;
-	int val1[] = {1, 2, 4};
+	int val1[] = {1, 4, 5};
 	int val2[] = {1, 3, 4};
-	struct ListNode *l1 = NULL, *l2 = NULL;
+	int val3[] = {2, 6};
+	struct ListNode *l1 = NULL, *l2 = NULL, *l3 = NULL;
 	struct ListNode *next = NULL, *tmp = NULL;
 	int listsSize = 3;
 	struct ListNode *lists[3];
@@ -37,6 +52,7 @@ int main()
 			tmp = next;
 		}
 	}
+	tmp->next = NULL;
 	for (i = 0; i < sizeof(val2) / sizeof(int); i++) {
 		next = (struct ListNode *)malloc(sizeof(struct ListNode));
 		next->val = val2[i];
@@ -49,6 +65,20 @@ int main()
 			tmp = next;
 		}
 	}
+	tmp->next = NULL;
+	for (i = 0; i < sizeof(val3) / sizeof(int); i++) {
+		next = (struct ListNode *)malloc(sizeof(struct ListNode));
+		next->val = val3[i];
+		next->next = NULL;
+		if (i == 0) {
+			l3 = next;
+			tmp = next;
+		} else {
+			tmp->next = next;
+			tmp = next;
+		}
+	}
+	tmp->next = NULL;
 
 	tmp = l1;
 	printf("list1 : ");
@@ -64,7 +94,17 @@ int main()
 		tmp = tmp->next;
 	}
 	printf("\n");
+	tmp = l3;
+	printf("list3 : ");
+	while (tmp != NULL) {
+		printf("%d ", tmp->val);
+		tmp = tmp->next;
+	}
+	printf("\n");
 
+	lists[0] = l1;
+	lists[1] = l2;
+	lists[2] = l3;
 	tmp = mergeKLists(lists, listsSize);
 	printf("merge list : ");
 	while (tmp != NULL) {
