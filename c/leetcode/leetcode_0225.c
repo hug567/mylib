7eb063f70db835111ffd577470b2d0f6438e7544
Huang Xing <huangxing567@163.com>
2019-11-18 16:09:30 -0800

    Create leetcode 225
    Signed-off-by: Huang Xing <huangxing567@163.com>

---
+++ c/leetcode/leetcode_0225.c
@@ -0,0 +1,18 @@
+/* leetcode 225: 用队列实现栈(中等) */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct {
+} MyStack;
+
+MyStack* myStackCreate() {
+}
+
+void myStackPush(MyStack* obj, int x) {
+}
+
+int main()
+{
+	return 0;
+}
