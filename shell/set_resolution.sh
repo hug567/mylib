a0a79c33177768a52c33f99dc9ca4011d5373de8
Huang Xing <huangxing567@163.com>
2019-12-12 06:51:38 -0800

    Add set_resolution.sh
    Signed-off-by: Huang Xing <huangxing567@163.com>

--- shell/set_resolution.sh
+++ shell/set_resolution.sh
@@ -6,6 +6,6 @@
 # 创建新的显示模式
 xrandr --newmode "1600x900_60.00"  118.25  1600 1696 1856 2112  900 903 908 934 -hsync +vsync
 # 添加新的显示模式
-sudo xrandr --addmoe Virtual1 "1600x900_60.00"
+sudo xrandr --addmode Virtual1 "1600x900_60.00"
 # 使用新添加的显示模式
 xrandr --output Virtual1 --mode 1600x900_60.00
