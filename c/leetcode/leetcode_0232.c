0908baac3a80e45cd6353a21b0f550ae97e0d510
Huang Xing <huangxing567@163.com>
2019-12-28 23:47:33 +0800

    Continue leetcode_0232.c
    Signed-off-by: Huang Xing <huangxing567@163.com>

--- c/leetcode/leetcode_0232.c
+++ c/leetcode/leetcode_0232.c
@@ -1,5 +1,6 @@
 /* leetcode 232: 用栈实现队列(简单) */
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
@@ -41,8 +42,8 @@ void myStackPush(MyStack *obj, int x)
 	} else {
 		ListNode *n = (ListNode *)malloc(sizeof(ListNode));
 		n->data = x;
-		n->next = obj->top->next;
-		obj->top->next = n;
+		n->next = obj->top;
+		obj->top = n;
 	}
 }
 
@@ -66,40 +67,75 @@ int myStackPeek(MyStack *obj)
 	return obj->top->data;
 }
 
-void myStackFree(MyStack *boj)
+void myStackFree(MyStack *obj)
 {
 	if (obj == NULL) {
 		return;
 	}
+	ListNode *tmp = obj->top;
+	while (tmp != NULL) {
+		obj->top = obj->top->next;
+		free(tmp);
+		tmp = obj->top;
+	}
+	free(obj);
 }
 
 typedef struct {
-
+	MyStack *s1;
+	MyStack *s2;
 } MyQueue;
 
 /** Initialize your data structure here. */
 
 MyQueue* myQueueCreate() {
-
-}
-
-/** Push element x to the back of queue. */
-void myQueuePush(MyQueue* obj, int x) {
-
-}
-
-/** Removes the element from in front of queue and returns that element. */
-int myQueuePop(MyQueue* obj) {
-
-}
-
-/** Get the front element. */
-int myQueuePeek(MyQueue* obj) {
-
+	MyQueue *obj = (MyQueue *)malloc(sizeof(MyQueue));
+	obj->s1 = myStackCreate();
+	obj->s1->top = NULL;
+	obj->s2 = myStackCreate();
+	obj->s2->top = NULL;
+	return obj;
 }
 
 /** Returns whether the queue is empty. */
 bool myQueueEmpty(MyQueue* obj) {
+	if (obj == NULL || obj->s1 == NULL || obj->s1->top == NULL) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+/** Push element x to the back of queue. */
+void myQueuePush(MyQueue* obj, int x) {
+	if (obj == NULL || obj->s1 == NULL) {
+		printf("queue is NULL\n");
+		return;
+	}
+	myStackPush(obj->s1, x);
+}
+
+/** Removes the element from in front of queue and returns that element. */
+int myQueuePop(MyQueue* obj) {
+	if (myQueueEmpty(obj)) {
+		printf("queue is empty\n");
+		return (int)(1U<<(8 * sizeof(int) - 1)); /* min int */
+	}
+	int tmp, x;
+	while (!myStackEmpty(obj->s1)) {
+		tmp = myStackPop(obj->s1);
+		myStackPush(obj->s2, tmp);
+	}
+	x = myStackPop(obj->s2);
+	while (!myStackEmpty(obj->s2)) {
+		tmp = myStackPop(obj->s2);
+		myStackPush(obj->s1, tmp);
+	}
+	return x;
+}
+
+/** Get the front element. */
+int myQueuePeek(MyQueue* obj) {
 
 }
 
@@ -123,5 +159,17 @@ void myQueueFree(MyQueue* obj) {
 
 int main()
 {
+	int i;
+
+	MyStack *s = myStackCreate();
+	for (i = 0; i < 10; i++) {
+		myStackPush(s, i);
+	}
+	printf("peek: %d\n", myStackPeek(s));
+	printf("stack: ");
+	for (i = 0; i < 10; i++) {
+		printf("%d ", myStackPop(s));
+	}
+	printf("\n");
 	return 0;
 }
