0ea27002e7142967bcf32bbc93e3f4f2cbb220a4
Huang Xing <huangxing567@163.com>
2020-01-02 23:16:30 +0800

    Add leetcode_0155.c
    Signed-off-by: Huang Xing <huangxing567@163.com>

---
+++ c/leetcode/leetcode_0155.c
@@ -0,0 +1,54 @@
+/* leetcode 155: 最小栈(简单) */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct {
+
+} MinStack;
+
+/** initialize your data structure here. */
+
+MinStack* minStackCreate() {
+
+}
+
+void minStackPush(MinStack* obj, int x) {
+
+}
+
+void minStackPop(MinStack* obj) {
+
+}
+
+int minStackTop(MinStack* obj) {
+
+}
+
+int minStackGetMin(MinStack* obj) {
+
+}
+
+void minStackFree(MinStack* obj) {
+
+}
+
+/**
+ * Your MinStack struct will be instantiated and called as such:
+ * MinStack* obj = minStackCreate();
+ * minStackPush(obj, x);
+
+ * minStackPop(obj);
+
+ * int param_3 = minStackTop(obj);
+
+ * int param_4 = minStackGetMin(obj);
+
+ * minStackFree(obj);
+*/
+
+int main(int argc, char *argv[])
+{
+	return 0;
+}
