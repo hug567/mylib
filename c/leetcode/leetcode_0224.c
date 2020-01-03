4da171dfa1ccd7d4e809e95233555a9983c85c92
Huang Xing <huangxing567@163.com>
2020-01-03 23:05:54 +0800

    Add leetcode_0224.c
    Signed-off-by: Huang Xing <huangxing567@163.com>

---
+++ c/leetcode/leetcode_0224.c
@@ -0,0 +1,16 @@
+/* leetcode 224: 基本计算器(困难) */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int calculate(char * s){
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	char str[] = "(1+(4+5+2)-3)+(6+8)";
+	printf("result = %d\n", calculate(str));
+	return 0;
+}
