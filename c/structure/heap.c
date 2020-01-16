48210b80c2d5d3002951c0ed5c9b1fa26d6d23d6
Huang Xing <huangxing567@163.com>
2020-01-16 22:11:22 +0800

    Continue heap.c
    Signed-off-by: Huang Xing <huangxing567@163.com>

--- c/structure/heap.c
+++ c/structure/heap.c
@@ -1,7 +1,26 @@
-/* 堆: 用数组实现的二叉树 */
+/*
+ * 堆: 用数组实现的二叉树
+ * 当前节点：i(i>0)
+ * 父节点：i/2
+ * 左子节点：2*i
+ * 右子节点：2*i+1
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define MAX_SIZE 100
+
+typedef struct {
+	int data[MAX_SIZE];
+	int count;
+} Heap;
+
+void heap_insert(Heap *h, int data)
+{
+	h->data[h->count] = data;
+	h->count++;
+}
 
 int main()
 {
