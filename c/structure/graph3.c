8bccfa784658d6aaae6044b52094a04a4f32f145
Huang Xing <huangxing567@163.com>
2020-01-18 14:45:49 +0800

    Add gtrph3.c
    Signed-off-by: Huang Xing <huangxing567@163.com>

---
+++ c/structure/graph3.c
@@ -0,0 +1,14 @@
+/* 无向图，边带权，十字链表 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_INT (int)((~0U) >> 1)
+#define MIN_INT (int)(1 << (8 * sizeof(int) - 1))
+#define MAX_SIZE 100
+
+int main()
+{
+	printf("hello graph3\n");
+	return 0;
+}
