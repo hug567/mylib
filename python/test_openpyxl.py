6082dd27299180bd64f70b695b98cdbcdc8b0728
Huang Xing <huangxing567@163.com>
2020-02-27 22:08:36 +0800

    Continue test_openpyxl.py
    Signed-off-by: Huang Xing <huangxing567@163.com>

--- python/test_openpyxl.py
+++ python/test_openpyxl.py
@@ -1,28 +1,37 @@
+###############################################################################
+# 功  能：python操作Excel
+# 版  本：python 3.6.3
+# 依赖包：openpyxl-3.0.3、jdcal-1.4.1、et_xmlfile-1.0.1
+# 时  间：2020-02-27
+###############################################################################
+
 # 导入openpyxl包
 import openpyxl
-
 from openpyxl import load_workbook
 
 # Excel文件名
 filename="test.xlsx"
 
 # 打开Excel，获取workbook
-workbook = load_workbook(filename)
+wb = load_workbook(filename)
 
-# 获取所有sheet名
-sheets_name = workbook.get_sheet_names()
-print("all sheet: ", end = "") # 输出不换行
-print(sheets_name)
+# 获取所有sheet
+sheets = wb.worksheets
+print("All sheets: ", end="")
+print(sheets)
 
-# 获取第一个sheet
-sheet = workbook.get_sheet_by_name(sheets_name[0].title())
+# 获取sheet
+sheet = sheets[0]
+print("Sheet title: " + sheet.title)
+
+# 获取表格
 cell = sheet['A1']
 print("A1 = %d" % cell.value)
 
 # 插入列
-sheet.insert_rows(1, 0)
+sheet.insert_rows(1)
 cell = sheet['A1']
 cell.value = "进度"
 
-# 保持workbook
-workbook.save("test2.xlsx")
+# 保存workbook
+wb.save("test2.xlsx")
