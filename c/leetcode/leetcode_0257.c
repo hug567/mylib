994ef98eb33bb4bcdb3bdb376c12da886a351563
Huang Xing <huangxing567@163.com>
2020-03-08 05:13:13 +0800

    Complete leetcode_0257.c
    Signed-off-by: Huang Xing <huangxing567@163.com>

--- c/leetcode/leetcode_0257.c
+++ c/leetcode/leetcode_0257.c
@@ -1,6 +1,7 @@
 /*
  * 题目：leetcode 257: 二叉树的所有路径
  * 难度：简单
+ * 技巧：dfs
  * 时间：2020-03-05
  * */
 #include <stdio.h>
@@ -38,22 +39,64 @@ void pre_order(struct TreeNode *tree)
 	pre_order(tree->right);
 }
 
+#define MAX_SIZE 100
+
+void dfs(struct TreeNode *tree, char **strs, int *path, int depth, int *returnSize)
+{
+	int i;
+	if (tree == NULL) {
+		return;
+	}
+	if (tree->left == NULL && tree->right == NULL) {
+		path[depth] = tree->val;
+		strs[*returnSize] = (char *)malloc(MAX_SIZE);
+		memset(strs[*returnSize], '\0', MAX_SIZE);
+		for (i = 0; i < depth; i++) {
+			sprintf(strs[*returnSize], "%s%d->", strs[*returnSize], path[i]);
+		}
+		sprintf(strs[*returnSize], "%s%d", strs[*returnSize], path[i]);
+		(*returnSize)++;
+	} else {
+		path[depth] = tree->val;
+		depth++;
+		dfs(tree->left, strs, path, depth, returnSize);
+		dfs(tree->right, strs, path, depth, returnSize);
+	}
+}
+
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
 char ** binaryTreePaths(struct TreeNode* root, int* returnSize){
+	int path[MAX_SIZE] = {0};
+	char **strs = (char **)malloc(MAX_SIZE * sizeof(char *));
 
+	*returnSize = 0;
+	if (root == NULL) {
+		return NULL;
+	}
+	dfs(root, strs, path, 0, returnSize);
+	return strs;
 }
 
 int main()
 {
+	int i;
 	int nums[] = {1, 2, 3, -1, 5};
 	int len = sizeof(nums) / sizeof(int);
 	struct TreeNode *tree = NULL;
+	char **strs = NULL;
+	int returnSize = 0;
 
 	create_tree(&tree, nums, len, 0);
+	printf("pre order: ");
 	pre_order(tree);
 	printf("\n");
+
+	strs = binaryTreePaths(tree, &returnSize);
+	for (i = 0; i < returnSize; i++) {
+		printf("strs[%d] = %s\n", i, strs[i]);
+	}
 
 	return 0;
 }
