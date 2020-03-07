488bb904408861f2edd05aea97347ee2aacbbf72
Huang Xing <huangxing567@163.com>
2020-03-08 05:37:08 +0800

    Contnue leetcode_0001.c
    Signed-off-by: Huang Xing <huangxing567@163.com>

--- c/leetcode/leetcode_0001.c
+++ c/leetcode/leetcode_0001.c
@@ -1,22 +1,47 @@
-/* leetcode 1: 两数之和(简单) */
+/*
+ * leetcode 1: 两数之和(简单)
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
 int* twoSum(int* nums, int numsSize, int target, int* returnSize){
-	int *sum = (int *)malloc(2 * sizeof(int));
+	if (nums == NULL) {
+		*returnSize = 0;
+		return NULL;
+	}
+	int i;
+	int j;
+	int *index = (int *)malloc(2 * sizeof(int));
 
-	*returnSize = 2;
-	return sum;
+	for (i = 0; i < numsSize - 1; i++) {
+		for (j = i + 1; j < numsSize; j++) {
+			if (nums[i] + nums[j] == target) {
+				index[0] = i;
+				index[1] = j;
+				*returnSize = 2;
+				return index;
+			}
+		}
+	}
+	*returnSize = 0;
+	return NULL;
 }
 
 int main()
 {
 	int i;
-	int sum = 0;
+	int nums[] = {2, 7, 11, 15};
+	int numsSize = sizeof(nums) / sizeof(int);
+	int target = 9;
+	int returnSize = 0;
+	int *index = NULL;
 
-	for (i = 1; i <= 100; i++) {
-		sum += i;
+	index = twoSum(nums, numsSize, target, &returnSize);
+	printf("sum index: ");
+	for (i = 0; i < returnSize; i++) {
+		printf("%d ", index[i]);
 	}
-	printf("sum = %d\n", sum);
+	printf("\n");
+
 	return 0;
 }
