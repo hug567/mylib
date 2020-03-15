3b92007bd27b87547780e9cea3dc6313f2e8d32a
Huang Xing <huangxing567@163.com>
2020-03-15 19:01:25 +0800

    Add test-color.sh

--- shell/qemu/mybuild-arm.sh
+++ shell/qemu/mybuild-arm.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+BLACK="\033[30m"
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+PURPLE="\033[35m"
+SKYBLUE="\033[36m"
+WHITE="\033[37m"
+RESET="\033[0m"
 
 make clean
 
@@ -8,4 +17,4 @@ make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- vexpress_defconfig
 
 make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi-
 
-echo -e "\nRun kernel: ~/code/mylib/shell/qemu/qemu-run.sh"
+echo -e "\n${YELLOW}Run kernel: ${GREEN}~/code/mylib/shell/qemu/qemu-run.sh${RESET}"
