739453bb7aabb56f31861027d1ee10f9c25e7779
Huang Xing <huangxing567@163.com>
2020-03-23 08:18:07 -0700

    Continue leetcode_0215.c
    Signed-off-by: Huang Xing <huangxing567@163.com>

--- c/leetcode/leetcode_0215.c
+++ c/leetcode/leetcode_0215.c
@@ -1,7 +1,7 @@
 /*
  * 题目：leetcode 215: 数组中第k大的元素
  * 难度：中等
- * 技巧：
+ * 技巧：维护一个k个不重复元素的最小堆
  * 时间：2020-03-15
  */
 #include <stdio.h>
@@ -18,7 +18,7 @@ struct Heap {
 
 struct Heap *CreateHeap()
 {
-	struct Heap *heap = (struct heap *)malloc(sizeof(struct Heap));
+	struct Heap *heap = (struct Heap *)malloc(sizeof(struct Heap));
 	heap->data = (int *)malloc((MAXSIZE + 1) * sizeof(int));
 	heap->size = 0;
 	heap->maxSize = MAXSIZE;
@@ -26,16 +26,73 @@ struct Heap *CreateHeap()
 
 void InsertNode(struct Heap *heap, int data)
 {
+	int i;
+	int tmp;
+
 	if (heap == NULL || heap->size >= heap->maxSize) {
 		return;
 	}
+
+	heap->size++;
+	heap->data[heap->size] = data;
+	i = heap->size;
+	while (i > 1) {
+		if (i / 2 >= 1 && heap->data[i] < heap->data[i / 2]) {
+			tmp = heap->data[i];
+			heap->data[i] = heap->data[i / 2];
+			heap->data[i / 2] = tmp;
+		} else {
+			break;
+		}
+		i /= 2;
+	}
+}
+
+void RefreshTop(struct Heap *heap, int data)
+{
+	int i = 1;
+	int tmp;
+
+	heap->data[1] = data;
+	while (i <= heap->size) {
+		if (2 * i <= heap->size && heap->data[i] > heap->data[2 * i]) {
+			tmp = heap->data[i];
+			heap->data[i] = heap->data[2 * i];
+			heap->data[2 * i] = tmp;
+		} else if ((2 * i + 1) <= heap->size && heap->data[i] > heap->data[2 * i + 1]) {
+			tmp = heap->data[i];
+			heap->data[i] = heap->data[2 * i + 1];
+			heap->data[2 * i + 1] = tmp;
+		} else {
+			return;
+		}
+	}
+}
+
+int NotExist(struct Heap *heap, int data)
+{
+	int i;
+	for (i = 1; i <= heap->size; i++) {
+		if (heap->data[i] == data) {
+			return 0;
+		}
+	}
+	return 1;
 }
 
 int findKthLargest(int* nums, int numsSize, int k){
+	int i;
 	struct Heap *heap = CreateHeap();
 
+	for (i = 0; i < numsSize; i++) {
+		if (heap->size < k && NotExist(heap, nums[i])) {
+			InsertNode(heap, nums[i]);
+		} else if (heap->size == k && heap->data[1] < nums[i]) {
+			RefreshTop(heap, nums[i]);
+		}
+	}
 
-	return 0;
+	return heap->data[1];
 }
 
 int main(int argc, char *argv[])
