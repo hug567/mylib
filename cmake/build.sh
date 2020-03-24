c9b5cbf4956a216bf30daa675d5ba30520a95e58
Huang Xing <huangxing567@163.com>
2020-03-24 09:07:08 -0700

    Add cmake for practice
    Signed-off-by: Huang Xing <huangxing567@163.com>

---
+++ cmake/build.sh
@@ -0,0 +1,14 @@
+OUTPUT=obj
+
+if [ -d ${OUTPUT} ]; then
+	rm -rf ${OUTPUT}
+fi
+mkdir ${OUTPUT}
+
+cp main.c ${OUTPUT}
+cp CMakeLists.txt ${OUTPUT}
+cp include.cmake ${OUTPUT}
+
+cd ${OUTPUT}
+cmake .
+make
---
+++ cmake/include.cmake
@@ -0,0 +1,1 @@
+message("[INFO]: Enter include.cmake...")
