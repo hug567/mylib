e11428efbd71991399cd06278d0d982a49fe845b
Huang Xing <huangxing567@163.com>
2020-03-29 16:00:21 +0800

    Complete leetcode_0295.c
    Signed-off-by: Huang Xing <huangxing567@163.com>

--- c/leetcode/leetcode_0295.c
+++ c/leetcode/leetcode_0295.c
@@ -1,7 +1,7 @@
 /*
  * 题目：leetcode 295: 数据流的中位数
  * 难度：困难
- * 技巧：使用链表保存数据
+ * 技巧：双向链表插入排序
  * 时间：2020-03-27
  */
 #include <stdio.h>
@@ -10,12 +10,12 @@
 
 struct Node {
 	int num;
+	struct Node *pre;
 	struct Node *next;
 };
 
 typedef struct {
 	struct Node *head;
-	struct Node *middle;
 	int len;
 } MedianFinder;
 
@@ -24,25 +24,72 @@ typedef struct {
 MedianFinder* medianFinderCreate() {
 	MedianFinder *obj = (MedianFinder *)malloc(sizeof(MedianFinder));
 	obj->head = NULL;
-	obj->middle = NULL;
 	obj->len = 0;
 	return obj;
 }
 
-void medianFinderAddNum(MedianFinder* obj, int num) {
-	struct Node *node = (struct Node *)malloc(sizeof(struct Node));
-	node->num = num;
-	node->next = NULL;
+static void InsertToHead(MedianFinder *obj, struct Node *node)
+{
 	if (obj->len == 0) {
 		obj->head = node;
-		obj->middle = node;
 	} else {
+		node->next = obj->head;
+		obj->head->pre = node;
+		obj->head = node;
 	}
 	obj->len++;
 }
 
-double medianFinderFindMedian(MedianFinder* obj) {
+static void InsertToMiddle(MedianFinder *obj, struct Node *node)
+{
+	struct Node *tmp = obj->head;
 
+	while (tmp->next != NULL) {
+		if (tmp->next->num >= node->num) {
+			break;
+		}
+		tmp = tmp->next;
+	}
+	if (tmp->next == NULL) { /* insert to tail */
+		tmp->next = node;
+		node->pre = tmp;
+	} else { /* insert to middle */
+		node->next = tmp->next;
+		tmp->next->pre = node;
+		tmp->next = node;
+		node->pre = tmp;
+	}
+	obj->len++;
+}
+
+void medianFinderAddNum(MedianFinder* obj, int num) {
+	int i;
+	int pos;
+	struct Node *tmp = NULL;
+	struct Node *node = (struct Node *)malloc(sizeof(struct Node));
+
+	node->num = num;
+	node->pre = NULL;
+	node->next = NULL;
+	if (obj->len == 0 || obj->head->num >= num) {
+		InsertToHead(obj, node);
+	} else {
+		InsertToMiddle(obj, node);
+	}
+}
+
+double medianFinderFindMedian(MedianFinder* obj) {
+	int i;
+	struct Node *tmp = obj->head;
+
+	for (i = 0; i < obj->len / 2; i++) {
+		tmp = tmp->next;
+	}
+	if (obj->len % 2 == 1) {
+		return tmp->num;
+	} else {
+		return (tmp->pre->num + tmp->num) / 2.0;
+	}
 }
 
 void medianFinderFree(MedianFinder* obj) {
@@ -68,5 +115,16 @@ void medianFinderFree(MedianFinder* obj) {
 
 int main(int argc, char *argv[])
 {
+	MedianFinder *obj = medianFinderCreate();
+
+	medianFinderAddNum(obj, -1);
+	medianFinderAddNum(obj, -2);
+	printf("mdiean = %f\n", medianFinderFindMedian(obj));
+	medianFinderAddNum(obj, -3);
+	medianFinderAddNum(obj, -4);
+	medianFinderAddNum(obj, -5);
+	printf("mdiean = %f\n", medianFinderFindMedian(obj));
+	medianFinderFree(obj);
+
 	return 0;
 }
