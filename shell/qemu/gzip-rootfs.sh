fa808a764ee19e8fb2c30046c140aed9879f6dc8
Huang Xing <huangxing567@163.com>
2020-04-26 00:12:23 +0800

    Continue practice linux-test
    Signed-off-by: Huang Xing <huangxing567@163.com>

--- shell/qemu/gzip-rootfs.sh
+++ shell/qemu/gzip-rootfs.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
-MYLIB="$(pwd)/../.."
+MYLIB="${HOME}/code/mylib"
 LINUX_TEST="${MYLIB}/linux/linux-test/linux-test.elf"
 BUSYBOX="${HOME}/code/linux/busybox-1.27.2"
 
 if [ -f ./rootfs.img ]; then
-    rm -rf ./rootfs.img
+    sudo rm -rf ./rootfs.img
 fi
 if [ -f ./rootfs.img.gz ]; then
-    rm -rf ./rootfs.img.gz
+    sudo rm -rf ./rootfs.img.gz
 fi
 if [ -d ./rootfs ]; then
-    rm -rf ./rootfs
+    sudo rm -rf ./rootfs
 fi
 
 mkdir -p rootfs
 cd rootfs
 sudo cp -rf ${BUSYBOX}/_install/* ./
 sudo mkdir proc sys dev etc etc/init.d
-sudo echo -e "#\!/bin/bash\nmount -t proc none /proc\nmount -t sysfs none /sys\n/sbin/mdev -s" > ./etc/init.d/rcS
+sudo touch ./etc/init.d/rcS
+sudo chmod a+x ./etc/init.d/rcS
+sudo sh -c 'echo "#!/bin/sh\nmount -t proc none /proc\nmount -t sysfs none /sys\n/sbin/mdev -s" > ./etc/init.d/rcS'
+
+sudo cp ${LINUX_TEST} ./
+sudo chmod a+x ${LINUX_TEST}
 
 find . | cpio -o --format=newc > ../rootfs.img
 cd ..
