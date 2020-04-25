95c38c962039c72a203c3c4a27e4afb1add6a344
Huang Xing <huangxing567@163.com>
2020-04-25 23:52:18 +0800

    Continue leetcode_0452.c
    Signed-off-by: Huang Xing <huangxing567@163.com>

--- c/leetcode/leetcode_0452.c
+++ c/leetcode/leetcode_0452.c
@@ -9,6 +9,8 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+#define LOCAL_DEBUG
+
 struct Node {
 	int start;
 	int end;
@@ -39,30 +41,50 @@ int CanShot(struct Node pt, int x)
 
 int findMinArrowShots(int** points, int pointsSize, int* pointsColSize){
 	int i;
-	int j;
-	int min = 1;
+	int end;
+	int min = 0;
+#ifdef LOCAL_DEBUG
 	/* 本地编译获取二维数组 */
 	int (*pt)[2] = (int(*)[2])points;
+#else
 	/* leetcode获取二维数组 */
-	//int **pt = points;
+	int **pt = points;
+#endif
 	struct Node *pts = (struct Node *)malloc(pointsSize * sizeof(struct Node));
 
 	for (i = 0; i < pointsSize; i++) {
 		pts[i].start = pt[i][0];
 		pts[i].end = pt[i][1];
 	}
-	PrintNode(pts, pointsSize);
+	//PrintNode(pts, pointsSize);
 	/* C语言快排库函数排序结构体数组 */
 	qsort(pts, pointsSize, sizeof(struct Node), compare);
-	PrintNode(pts, pointsSize);
+	//PrintNode(pts, pointsSize);
+
+    i = 0;
+    end = pts[0].end;
+    while (i < pointsSize) {
+        while (i < pointsSize && CanShot(pts[i], end)) {
+            i++;
+        }
+        min++;
+        i++;
+        if (i < pointsSize) {
+            end = pts[i].end;
+        } else if (i == pointsSize) {
+            min++;
+        }
+    }
+    printf("i = %d\n", i);
 
 	*pointsColSize = 2;
-	return 0;
+	return min;
 }
 
 int main(int argc, char *argv[])
 {
-	int points[][2] = {{10, 16}, {2, 8}, {1, 6}, {7, 12}};
+	//int points[][2] = {{10, 16}, {2, 8}, {1, 6}, {7, 12}};
+	int points[][2] = {{1, 2}, {3, 4}, {5, 6}, {7, 8}};
 	int pointsSize = 4;
 
 	int pointsColSize;
