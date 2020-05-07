b1b5c6d5526f08638a7130ba615061377ec9c36f
Huang Xing <huangxing567@163.com>
2020-05-08 00:26:27 +0800

    Update gzip-rootfs.sh
    Signed-off-by: Huang Xing <huangxing567@163.com>

--- shell/qemu/gzip-rootfs.sh
+++ shell/qemu/gzip-rootfs.sh
@@ -6,28 +6,42 @@ MYLIB="${HOME}/code/mylib"
 LINUX_TEST="${MYLIB}/linux/linux-test/linux-test.elf"
 BUSYBOX="${HOME}/code/linux/busybox-1.27.2"
 WORK_DIR="${HOME}/code/linux"
+TEST_ELF="${MYLIB}/c/test-libc/obj/test-libc.elf"
 
 if [ -f "${WORK_DIR}/rootfs.img" ]; then
+    echo "delete old file ${WORK_DIR}/rootfs.img"
     sudo rm -rf ${WORK_DIR}/rootfs.img
 fi
 if [ -f "${WORK_DIR}/rootfs.img.gz" ]; then
+    echo "delete old file ${WORK_DIR}/rootfs.img.gz"
     sudo rm -rf ${WORK_DIR}/rootfs.img.gz
 fi
 if [ -d "${WORK_DIR}/rootfs" ]; then
+    echo "delete old dir ${WORK_DIR}/rootfs/"
     sudo rm -rf ${WORK_DIR}/rootfs
 fi
 
 mkdir -p ${WORK_DIR}/rootfs
 cd ${WORK_DIR}/rootfs
+
+echo "copy busybox file to rootfs"
 sudo cp -rf ${BUSYBOX}/_install/* ./
 sudo mkdir -p proc sys dev etc etc/init.d tmp
 sudo touch ./etc/init.d/rcS
 sudo chmod a+x ./etc/init.d/rcS
 sudo sh -c 'echo "#!/bin/sh\nmount -t proc none /proc\nmount -t sysfs none /sys\n/sbin/mdev -s" > ./etc/init.d/rcS'
 
+echo "copy {LINUX_TEST} file to rootfs"
 sudo cp ${LINUX_TEST} ./
 sudo chmod a+x ${LINUX_TEST}
 
+echo "copy ${TEST_ELF} file to rootfs"
+sudo cp ${TEST_ELF} ./
+sudo chmod a+x ${TEST_ELF}
+
+echo "package rootfs.img"
 find . | cpio -o --format=newc > ../rootfs.img
+
+echo "gzip rootfs.img.gz"
 cd ..
 gzip -c rootfs.img > rootfs.img.gz
