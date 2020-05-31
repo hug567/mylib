0ecef41ee0cbb19ed5635dac957e36d7b4ddfe35
Huang Xing <huangxing567@163.com>
2020-05-31 23:47:43 +0800

    Continue build linux
    Signed-off-by: Huang Xing <huangxing567@163.com>

--- linux/shell/build-qemu-arm.sh
+++ linux/shell/build-qemu-arm.sh
@@ -9,14 +9,19 @@ SKYBLUE="\033[36m"
 WHITE="\033[37m"
 RESET="\033[0m"
 
+linux_dir="${HOME}/code/linux/linux-4.15"
+
 # 清空屏幕
 clear; clear
 
-#
-make clean
+# 删除编译生成文件
+#make clean
 
-#
-make mrproper
+# 删除编译生成文件与配置文件
+#make mrproper
+
+# 进入源码目录
+cd ${linux_dir}
 
 # 生成.config配置文件
 make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- vexpress_defconfig
@@ -25,10 +30,10 @@ make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- vexpress_defconfig
 make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi-
 
 # 编译内核模块(.ko)
-make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- modules
+#make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- modules
 
 # 编译dts文件(生成dtb文件)
-make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- dtbs
+#make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- dtbs
 
 # qemu启动命令
 echo -e "\n${YELLOW}Run kernel: ${GREEN}~/code/mylib/shell/qemu/qemu-run.sh${RESET}"
