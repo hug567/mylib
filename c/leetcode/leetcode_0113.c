56ad1b6c13b7f7d9f8acd4a2d80be59c5be7df0d
Huang Xing <huangxing567@163.com>
2020-06-08 23:08:03 +0800

    Add leetcode_0113.c
    Signed-off-by: Huang Xing <huangxing567@163.com>

---
+++ c/leetcode/leetcode_0113.c
@@ -0,0 +1,34 @@
+/*
+ * 题目：leetcode 113: 路径总和II
+ * 难度：中等
+ * 技巧：
+ * 时间：2020-06-08
+ */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define LOCAL_DEBUG
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** pathSum(struct TreeNode* root, int sum, int* returnSize, int** returnColumnSizes){
+    return NULL;
+}
+
+void CreateTree()
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
