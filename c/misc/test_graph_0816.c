3710d05583aeedb324d5c9a8e209f3a20b9b7d6c
Huang Xing <huangxing567@163.com>
2020-08-19 00:09:00 +0800

    Continue learn graph
    Signed-off-by: Huang Xing <huangxing567@163.com>

--- c/misc/test_graph_0816.c
+++ c/misc/test_graph_0816.c
@@ -1,10 +1,33 @@
 /*
- * 练习：有向图，邻接矩阵
+ * 练习：无向图，邻接矩阵
  * 2020-08-16
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define MAX_SIZE 100
+#define MAX_NUM 65535
+
+struct Graph {
+    int vex[MAX_SIZE];
+    int arc[MAX_SIZE][MAX_SIZE];
+    int numVex;
+    int numArc;
+};
+
+void InitGraph(struct Graph *g)
+{
+    int i, j;
+    for (i = 0; i < MAX_SIZE; i++) {
+        g->vex[i] = MAX_NUM;
+        for (j = 0; j < MAX_SIZE; j++) {
+            g->arc[i][j] = MAX_NUM;
+        }
+    }
+    g->numVex = 0;
+    g->numArc = 0;
+}
 
 int main(void)
 {
