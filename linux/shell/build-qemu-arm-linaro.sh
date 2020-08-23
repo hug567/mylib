9ef6ac97153c7c34116c3ac4a0e5ed88e5e00a22
Huang Xing <huangxing567@163.com>
2020-08-23 22:53:30 +0800

    Add build-qemu-arm-linaro.sh
    Signed-off-by: Huang Xing <huangxing567@163.com>

---
+++ linux/shell/build-qemu-arm-linaro.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+BLACK="\033[30m"
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+PURPLE="\033[35m"
+SKYBLUE="\033[36m"
+WHITE="\033[37m"
+RESET="\033[0m"
+
+# 清空屏幕
+clear; clear
+
+if [ ! -f ./include/linux/kernel.h ]; then
+    echo -e "${RED}Error: ${RESET}You are not in linux src root dir....."
+    exit
+fi
+
+# 删除编译生成文件
+#make clean
+
+# 删除编译生成文件与配置文件
+#make mrproper
+
+# 生成.config配置文件
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- vexpress_defconfig
+
+# 编译内核
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-
+
+# 编译内核模块(.ko)
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- modules
+
+# 编译dts文件(生成dtb文件)
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- dtbs
+
+# qemu启动命令
+echo -e "\n${GREEN}Run kernel: ${RESET}~/code/mylib/shell/qemu/qemu-run.sh"
--- linux/shell/run-qemu-arm.sh
+++ linux/shell/run-qemu-arm.sh
@@ -2,19 +2,15 @@
 
 ROOTFS="${HOME}/code/linux/rootfs.img.gz"
 
-if [ ! -f ./include/linux/kernel.h ]; then
+if [ ! -f arch/arm/boot/zImage ]; then
     echo -e "${RED}Error: ${RESET}You are not in linux src root dir....."
     exit
 fi
 
 qemu-system-arm \
-    -M virt  \
-    -smp 1 \
-    -m 256 \
-    -kernel ./arch/arm/boot/zImage \
+    -M virt -smp 1 -m 256 -nographic -s \
+    -kernel arch/arm/boot/zImage \
     -initrd ${ROOTFS} \
-    -nographic \
     -netdev tap,id=mynet,script=no,downscript=no,ifname=tap0 \
     -device virtio-net-device,netdev=mynet,mrg_rxbuf=off,csum=off,guest_csum=off,gso=off,guest_tso4=off,guest_tso6=off,guest_ecn=off,guest_ufo=off \
     -append "root=/dev/mtdblock0 rdinit=sbin/init console=ttyAMA0 noapic"
-#    -net nic,model=e1000 -net tap,ifname=tap0 \
