2de0b27a086e1feb48a4f5df91e777003835d6ce
Huang Xing <huangxing567@163.com>
2020-09-01 00:02:57 +0800

    Add settings.json of vscode
    Signed-off-by: Huang Xing <huangxing567@163.com>

--- c/misc/test_graph_0824.c
+++ c/misc/test_graph_0824.c
@@ -8,13 +8,13 @@
 #define MAX_VEX 100
 
 struct ArcNode {
-    int vex;
+    int adjIndex; /* 弧的邻接点在顶点数组中的下标 */
     int weight;
     struct ArcNode *next;
 };
 
 struct VexNode {
-    int vex;
+    int val;
     struct ArcNode *first;
 };
 
@@ -29,7 +29,7 @@ void InitGraph(struct Graph *g)
     int i;
 
     for (i = 0; i < MAX_VEX; i++) {
-        g->vexs[i].vex = 0;
+        g->vexs[i].val = 0;
         g->vexs[i].first = NULL;
     }
     g->numVex = 0;
@@ -41,11 +41,11 @@ int VexIndex(struct Graph *g, int vex)
     int i;
 
     for (i = 0; i < g->numVex; i++) {
-        if (vex == g->vexs[i].vex) {
+        if (vex == g->vexs[i].val) {
             return i;
         }
     }
-    g->vexs[i].vex = vex;
+    g->vexs[i].val = vex;
     g->numVex++;
     return i;
 }
@@ -53,11 +53,46 @@ int VexIndex(struct Graph *g, int vex)
 void CreateGraphByArray(struct Graph *g, int **arr_t, int size)
 {
     int i;
+    int m, n;
     int (*arr)[3] = (int(*)[3])arr_t;
+    struct ArcNode *arc = NULL;
+    struct ArcNode *temp = NULL;
+
+    for (i = 0; i < size; i++) {
+        m = VexIndex(g, arr[i][0]);
+        n = VexIndex(g, arr[i][1]);
+        arc = (struct ArcNode *)malloc(sizeof(struct ArcNode));
+        arc->adjIndex = n;
+        arc->weight = arr[i][2];
+        arc->next = NULL;
+        temp = g->vexs[m].first;
+        if (temp == NULL) {
+            g->vexs[m].first = arc;
+        } else {
+            while (temp->next != NULL) {
+                temp = temp->next;
+            }
+            temp->next = arc;
+        }
+        g->numArc++;
+    }
 }
 
 void PrintGraph(struct Graph *g)
-{}
+{
+    int i;
+    struct ArcNode *arc = NULL;
+
+    for (i = 0; i < g->numVex; i++) {
+        arc = g->vexs[i].first;
+        printf("%d: ", g->vexs[i].val);
+        while (arc != NULL) {
+            printf("%d->%d ", g->vexs[i].val, g->vexs[arc->adjIndex].val);
+            arc = arc->next;
+        }
+        printf("\n");
+    }
+}
 
 int main(void)
 {
