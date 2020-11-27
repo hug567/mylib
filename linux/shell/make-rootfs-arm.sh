2f91d2f49a94765a4ea2f786171b645f8bf92502
Huang Xing <huangxing567@163.com>
2020-11-28 00:41:26 +0800

    Add lib rootfs file
    Signed-off-by: Huang Xing <huangxing567@163.com>

--- linux/shell/make-rootfs-arm.sh
+++ linux/shell/make-rootfs-arm.sh
@@ -4,6 +4,7 @@ BASE_DIR=${HOME}/code/linux
 MYLIB=${HOME}/code/mylib
 BUSYBOX_DIR=${BASE_DIR}/busybox-1.27.2
 CROCESS_COMPILER_DIR=${HOME}/tools/gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabi
+LIB_ROOTFS_DIR=${MYLIB}/linux/rootfs
 ROOTFS=rootfs.sd
 
 cd ${BASE_DIR}
@@ -14,23 +15,15 @@ mkdir rootfs
 cd rootfs
 
 echo "[INFO]: copy busybox file to rootfs"
+# bin  linuxrc  sbin  usr
 sudo cp -rf ${BUSYBOX_DIR}/_install/* ./
-sudo mkdir -p proc sys tmp root dev etc/init.d usr/bin lib/modules
-sudo touch etc/init.d/rcS
+sudo mkdir -p proc sys tmp root dev/pts etc/init.d usr/bin lib/modules
+sudo cp ${LIB_ROOTFS_DIR}/etc/init.d/rcS etc/init.d/
+sudo cp ${LIB_ROOTFS_DIR}/etc/profile etc/
 sudo chmod a+x etc/init.d/rcS
-sudo sh -c 'echo "#!/bin/sh" > etc/init.d/rcS'
-sudo sh -c 'echo "mount -t proc proc /proc" >> etc/init.d/rcS'
-sudo sh -c 'echo "mount -t sysfs sysfs /sys" >> etc/init.d/rcS'
-sudo sh -c 'echo "mount -t tmpfs tmpfs /dev" >> etc/init.d/rcS'
-sudo sh -c 'echo "mkdir -p /dev/pts" >> etc/init.d/rcS'
-sudo sh -c 'echo "mount -t devpts devpts /dev/pts" >> etc/init.d/rcS'
-sudo sh -c 'echo "/sbin/mdev -s" >> etc/init.d/rcS'
-sudo sh -c 'echo "echo /sbin/mdev > /proc/sys/kernel/hotplug" >> etc/init.d/rcS'
-sudo sh -c 'echo "telnetd" >> etc/init.d/rcS'
-sudo sh -c 'echo "ifconfig eth0 192.168.0.101 netmask 255.255.255.0" >> etc/init.d/rcS'
+sudo chmod a+x etc/profile
 
-sudo touch etc/passwd
-sudo sh -c 'echo "root::0:0:root:/root:/bin/sh" > etc/passwd'
+sudo cp ${LIB_ROOTFS_DIR}/etc/passwd etc/
 
 echo "[INFO]: mknod tty device"
 cd dev
