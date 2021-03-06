a11fabd118eb5d68b6d9371bd133772554ffe767
Huang Xing <huangxing567@163.com>
2021-03-06 23:55:45 +0800

    Complete c/misc/eight_queen.c
    Signed-off-by: Huang Xing <huangxing567@163.com>

--- c/misc/eight_queen.c
+++ c/misc/eight_queen.c
@@ -1,205 +1,88 @@
 /*
  * 八皇后问题：在8X8格的国际象棋上摆放八个皇后（棋子），使其不能互相攻击，
  *            即任意两个皇后都不能处于同一行、同一列或同一斜线上。
- * 回溯算法：结合递归
+ * 解法一：回溯算法，结合递归（未验证）
+ * 解法二：DFS，基于递归实现
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define log(fmt, ...) printf("[%s:%d] " fmt, __func__, __LINE__, ##__VA_ARGS__)
+#define DebugLog(fmt, ...) {if (1) {printf("[%s:%d] " fmt, __func__, __LINE__, ##__VA_ARGS__);}}
 
 #define N 8 /* 棋盘尺寸 */
 
-int g_board[N][N] = {0}; /* 棋盘 */
-int g_col[N] = {0}; /* 每行皇后所在列 */
+int g_queue[N] = {0}; /* 每行皇后所在列 */
+int g_count = 0; /* 可以的方案总数 */
 
-void print_board(int size)
+void PrintBoard(void)
 {
     int i, j;
     printf("board:\n");
-    for (i = 0; i < size; i++) {
-        for (j = 0; j < size; j++) {
-            printf("%d ", g_board[i][i]);
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            int val = 0;
+            if (g_queue[i] == j) {
+                val = 1;
+            }
+            printf("%d ", val);
         }
         printf("\n");
     }
 }
 
-int can_set(int row, int col)
-{
-    int i, j;
-
-    /* 同一横线是否有皇后 */
-    for (j = 0; j < N; j++) {
-        if (g_board[row][j] == 1) {
-            return 0;
-        }
-    }
-
-    /* 同一竖线是否有皇后 */
-    for (i = 0; i < N; i++) {
-        if (g_board[i][col] == 1) {
-            return 0;
-        }
-    }
-
-    /* 左上斜线是否有皇后 */
-    for(i = row, j = col; i >= 0 && j >= 0; i--, j--) {
-        if (g_board[i][j] == 1) {
-            return 0;
-        }
-    }
-
-    /* 左下斜线是否有皇后 */
-    for(i = row, j = col; i < N && j >= 0; i++, j--) {
-        if (g_board[i][j] == 1) {
-            return 0;
-        }
-    }
-
-    /* 右上斜线是否有皇后 */
-    for(i = row, j = col; i >= 0 && j < N; i--, j++) {
-        if (g_board[i][j] == 1) {
-            return 0;
-        }
-    }
-
-    /* 右下斜线是否有皇后 */
-    for(i = row, j = col; i < N && j < N; i++, j++) {
-        if (g_board[i][j] == 1) {
-            return 0;
-        }
-    }
-
-    return 1;
-}
-
-/* 清除指定行的皇后, 用于回溯 */
-void clear_row(int row)
+/*
+ * 当前点能否放置皇后，前0~row-1行的皇后均已放置，且每行皇后所在列
+ * 存储在g_queue[N]数组中
+ */
+int CanSet(int row, int col)
 {
     int i;
-    for (i = 0; i < N; i++) {
-        g_board[row][i] = 0;
-    }
-}
 
-/* 指定行皇后移动至下一列 */
-void move_to_next_col(int row, int size)
-{
-    int i;
-    for (i = 0; i < size - 1; i++) {
-        if (g_board[row][i] = 1) {
-            g_board[row][i] = 0;
-            g_board[row][i + 1] = 1;
-            return;
+    /* 遍历每行皇后与当前点是否在同一列或同一斜线，肯定不在同一行 */
+    for (i = 0; i < row; i++) {
+        /*
+         * 在同一列：col相同
+         * 在同一斜线：行列坐标差值相同
+         */
+        if (g_queue[i] == col || (row - i) == (col - g_queue[i]) ||
+            (row - i) == (g_queue[i] - col)) {
+            return 0; /* 不可放置 */
         }
     }
+    return 1; /* 可以放置 */
 }
 
-/* size: 棋盘大小 */
-void search(int size)
+int g_flag = 1;
+/* DFS: 搜索当前row行可放置皇后的位置 */
+void search(int row)
 {
     int i;
-    int cur = 0; /* 当前遍历的行 */
 
-    g_col[0] = 0;  /* 初始调试，第一个皇后放在顶点处 */
-    g_board[0][0] = 1;
-    while (cur < size) {
-        cur++;
-        for (i = 0; i < size; i++) {
-            if (can_set(cur, i)) {
-                g_col[cur] = i;
-                g_board[cur][i] = 1;
-                break;
-            }
+    if (row == N) { /* 0~N-1行均放置了皇后，为满足条件的方案 */
+        g_count++;
+        if (g_flag == 1) {
+            PrintBoard();
+            g_flag = 0;
         }
-        if (i < size) { /* 可以放置，遍历下一个皇后 */
-            continue;
-        } else {
-            //clear_row(cur);
-            cur--;
-            g_board[cur][g_col[cur]] = 0;
-            g_col[cur]++;
-            if (g_col[cur] >= size) {
-                printf("search failed\n");
-                return;
-            }
-            g_board[cur][g_col[cur]] = 1;
-            //move_to_next_col(cur, size);
-        }
-    }
-}
-
-int g_count = 0;
-/* 结合递归的回溯搜索 */
-void search2(int row)
-{
-    int i;
-    if (row == 8) {
         return;
     }
-//log("g_count = %d\n", g_count);
-g_count++;
-    for (i = 0; i < 8; i++) {
-        if (can_set(row, i)) {
-            g_col[row] = i;
-            g_board[row][i] = 1;
-            if (row == 7) {
-                print_board(8);
-                g_col[row] = 0;
-                g_board[row][g_col[row]] = 0;
-                return;
-            }
+
+    /* 遍历当前行哪个位置可放置皇后 */
+    for (i = 0; i < N; i++) {
+        int canSet = CanSet(row, i); /* 当前点(row, i)能否放置皇后 */
+        if (canSet) { /* 可以放置 */
+            g_queue[row] = i; /* 放置皇后 */
+            search(row + 1); /* 搜索下一行 */
         }
-        search2(row + 1);
-        g_col[row] = 0;  /* 回溯 */
-        g_board[row][g_col[row]] = 0;
     }
 }
 
 int main(void)
 {
-    /* 可放置皇后 */
-    printf("cat set(1, 1) = %d(1)\n", can_set(1, 1));
 
-    /* 同一横线有皇后 */
-    g_board[1][2] = 1;
-    printf("cat set(1, 5) = %d(0)\n", can_set(1, 5));
-    g_board[1][2] = 0;
-
-    /* 同一竖线有皇后 */
-    g_board[1][2] = 1;
-    printf("cat set(4, 2) = %d(0)\n", can_set(4, 2));
-    g_board[1][2] = 0;
-
-    /* 左上竖线有皇后 */
-    g_board[1][1] = 1;
-    printf("cat set(4, 4) = %d(0)\n", can_set(4, 4));
-    g_board[1][1] = 0;
-
-    /* 左下竖线有皇后 */
-    g_board[0][7] = 1;
-    printf("cat set(7, 0) = %d(0)\n", can_set(7, 0));
-    g_board[0][7] = 0;
-
-    /* 右上竖线有皇后 */
-    g_board[7][0] = 1;
-    printf("cat set(0, 7) = %d(0)\n", can_set(0, 7));
-    g_board[7][0] = 0;
-
-    /* 右下竖线有皇后 */
-    g_board[7][7] = 1;
-    printf("cat set(3, 3) = %d(0)\n", can_set(3, 3));
-    g_board[7][7] = 0;
-
-    /* 可放置皇后 */
-    g_board[1][1] = 1;
-    printf("cat set(0, 4) = %d(1)\n", can_set(0, 4));
-    g_board[1][1] = 0;
-
-    search2(0);
-    //print_board(N);
+    search(0); /* 从第0行开始搜索 */
+    printf("g_count = %d(92)\n", g_count);
 
     return 0;
 }
--- mynote/software_use.md
+++ mynote/software_use.md
@@ -2,8 +2,56 @@
 
 ```shell
 # 常用快捷键：
-Ctrl + Shift + B                                 # 显示/隐藏侧边栏
-Ctrl + L                                         # 清除画面
-Ctrl + Tab                                       # 向前导航窗口
-Ctrl + Shift + Tab                               # 向后导航
+Ctrl + Shift + B                                                     # 显示/隐藏侧边栏
+Ctrl + L                                                             # 清除画面
+Ctrl + Tab                                                           # 向前导航窗口
+Ctrl + Shift + Tab                                                   # 向后导航
+```
+
+## 2、端口转发：
+
+### 1)、Linux设置端口转发：
+
+```shell
+socat TCP4-LISTEN:1234,reuseaddr,fork TCP4:192.168.0.1:80 &          # 设置端口转发
+netstat -antup                                                       # 查看所有转发端口
+ps aux | grep socat                                                  # 查看端口转发后台进程
+kill <PID>                                                           # 终止转发进程
+```
+
+### 2)、Windows设置端口转发：
+
+```shell
+# 以管理员身份打开powershell:
+# 远程主机设置端口转发
+netsh interface portproxy add v4tov4 listenport=1234 connectaddress=192.168.0.1 connectport=80
+netsh interface portproxy show all                                   # 查看所有端口转发
+netsh interface portproxy delete v4tov4 listenport=1234              # 删除转发的端口
+http://101.45.92.102:1234                                            # 本机打开远程主机映射端口
+```
+
+### 3)、ssh设置端口转发：需可ssh连接远程主机
+
+```shell
+ssh -L 0.0.0.0:1234:192.168.0.1:80 user@101.45.92.102                # 本机转发远程主机端口
+http://localhost:1234                                                # 本机浏览器打开
+```
+
+### 4)、Windows安装ssh:
+
+```shell
+# 在线安装：
+设置 -> 应用 -> 应用和功能 -> 可选功能 -> 添加功能 -> OpenSSH 服务器 -> 安装
+
+# 离线安装：
+https://github.com/PowerShell/Win32-OpenSSH/releases                 # 下载opensSSH
+C:\Program Files\OpenSSH                                             # 解压到安装目录
+# 启动ssh-agnet
+服务 -> OpenSSH Authentication Agent -> 启动类型改为“手动” -> 右键设置启动
+# 以管理员身份运行powershell并进入OpenSSH安装目录：
+.\FixHostFilePermissions.ps1                                         # 执行修复脚本
+Start-Service sshd                                                   # 启动sshd
+Set-Service -Name ssh -StartupType 'Automatic'                       # 设置自启动
+
+ssh user@101.45.92.102                                               # ssh连接Windows
 ```
