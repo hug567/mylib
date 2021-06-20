95aa2429105c827f7e73b6e83f3608dc48395e01
Huang Xing <huangxing567@163.com>
2021-06-20 10:28:17 +0800

    Update .vimrc
    Signed-off-by: Huang Xing <huangxing567@163.com>

--- cmake/script/common.cmake
+++ cmake/script/common.cmake
@@ -36,3 +36,22 @@ function(mt_file_var var)
     string(REPLACE "\n" "" cur_time ${cur_time})
     file(APPEND ${log_file} "[${cur_time}][INFO ]: ${var} = ${${var}}\n")
 endfunction(mt_file_var)
+
+
+# 单个文件编译成单个文件的测试用例
+# 文件名包可含：大小写字母、数字、下划线、横线
+# eg:
+# compile_single_file(
+#     file1.c
+#     file2.c
+# )
+function(compile_single_file)
+    set(index 0)
+    while(index LESS ${ARGC})
+        set(file ${ARGV${index}})
+        string(REGEX MATCH "[A-Za-z0-9_-]+\.c$" file_bin ${file})
+        string(REGEX MATCH "^[A-Za-z0-9_-]+" file_bin ${file_bin})
+        add_executable(${file_bin} ${file})
+        math(EXPR index "${index} + 1")
+    endwhile()
+endfunction(compile_single_file)
