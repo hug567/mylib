ad1acd3a7c475ec102c071f3f6d76e2297648e91
Huang Xing <huangxing567@163.com>
2021-09-23 23:28:24 +0800

    Continue practice tree bfs
    Signed-off-by: Huang Xing <huangxing567@163.com>

--- c/misc/tree_20210916.c
+++ c/misc/tree_20210916.c
@@ -4,6 +4,9 @@
  */
 #include "common.h"
 
+#define INT_MAX ((int)((~0U) >> 1))
+#define INT_MIN ((int)(1U << 31))
+
 struct TreeNode {
     int val;
     struct TreeNode *left;
@@ -80,6 +83,97 @@ struct TreeNode *CreateTreeByArray(int *arr, int size)
     }
 }
 
+void PrintTreePath(int *path, int len)
+{
+    int i;
+    DebugLog("path: ");
+    for (i = 0; i < len; i++) {
+        printf("%d ", path[i]);
+    }
+    printf("\n");
+}
+
+int g_num[10];
+void Dfs(struct TreeNode *t, int depth)
+{
+    if (t == NULL) {
+        return;
+    }
+    g_num[depth] = t->val;
+    if (t->left == NULL && t->right == NULL) {
+        PrintTreePath(g_num, depth + 1);
+    } else {
+        Dfs(t->left, depth + 1);
+        Dfs(t->right, depth + 1);
+    }
+}
+
+int TreeDepth(struct TreeNode *t)
+{
+    if (t == NULL) {
+        return 0;
+    }
+    int ld = TreeDepth(t->left);
+    int rd = TreeDepth(t->right);
+    return (ld > rd ? ld : rd) + 1;
+}
+
+#define MAX_NODE 100
+struct TreeNode *g_queue[MAX_NODE] = {NULL};
+int g_head = 0;
+int g_tail = 0;
+
+int QueueEmpty(void)
+{
+    return (g_head == g_tail);
+}
+
+int QueueFull(void)
+{
+    return ((g_tail + 1) % MAX_NODE == g_head);
+}
+
+void EnQueue(struct TreeNode *t)
+{
+    if (t == NULL || QueueFull()) {
+        return;
+    }
+    g_queue[g_tail] = t;
+    g_tail = (g_tail + 1) % MAX_NODE;
+}
+
+struct TreeNode *DeQueue(void)
+{
+    struct TreeNode *head = NULL;
+    if (QueueEmpty()) {
+        return NULL;
+    }
+    head = g_queue[g_head];
+    g_head = (g_head + 1) % MAX_NODE;
+    return head;
+}
+
+void Bfs(struct TreeNode *t)
+{
+    struct TreeNode *tmp = NULL;
+    if (t == NULL) {
+        return;
+    }
+    EnQueue(t);
+    DebugLog("bfs: ");
+    while (!QueueEmpty()) {
+        tmp = DeQueue();
+        printf("%d ",  tmp->val);
+        if (tmp->left != NULL) {
+            EnQueue(tmp->left);
+        }
+        if (tmp->right != NULL) {
+            EnQueue(tmp->right);
+        }
+    }
+    printf("\n");
+}
+
 int main(void)
 {
     int arr[] = {5, 4, 11, 7, -1, -1, 2, -1, -1, -1, 8,
@@ -87,9 +181,17 @@ int main(void)
     int size = sizeof(arr) / sizeof(arr[0]);
     struct TreeNode *t = CreateTreeByArray(arr, size);
 
+    DebugLog("INT_MAX = %d\n", INT_MAX);
+    DebugLog("INT_MIN = %d\n", INT_MIN);
+
+    DebugLog("tree depth: %d\n", TreeDepth(t));
     PreOrder(t);
     InOrder(t);
     PostOrder(t);
+
+    Dfs(t, 0);
+
+    Bfs(t);
 
     return 0;
 }
