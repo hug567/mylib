fdb2c057f4844d3b851d82454e626e79bec675e9
Huang Xing <huangxing567@163.com>
2021-11-15 23:32:57 +0800

    Continue learn python list
    Signed-off-by: Huang Xing <huangxing567@163.com>

--- python/test/test_list.py
+++ python/test/test_list.py
@@ -58,3 +58,47 @@ print(subjects)
 # 反转列表元素
 subjects.reverse()
 print(subjects)
+
+# 遍历链表
+for sub in subjects:
+    print("subject: " + sub)
+
+# range函数
+for value in range(1, 6):
+    print(value) # 1, 2, 3, 4, 5
+# 创建数值列表
+numbers = list(range(1, 6))
+print(numbers)
+
+# 数值列表计算
+print("min(numbers) =", min(numbers))
+print("max(numbers) =", max(numbers))
+print("sum(numbers) =", sum(numbers))
+
+# 列表解析：使用for循环创建列表
+squares=[value ** 2 for value in range(1, 11)] # value ^ 2
+print(squares)
+
+# 列表切片
+print(squares[1:4]) # 1-3的成员
+print(squares[:4]) # 0-3的成员
+print(squares[4:]) # 4-末尾的成员
+print(squares[-3:]) # 最后3个成员
+
+# 复制列表
+squares2 = squares[:]
+print(squares2)
+
+# 元组：值不可变的列表
+size = (10, 50, 47)
+print("size[0] :", size[0])
+print("size[1] :", size[1])
+print("size[2] :", size[2])
+
+# 遍历元组
+for s in size:
+    print("size :", s)
+
+# 重新复制元组
+size = (20, 30, 62)
+print(size)
