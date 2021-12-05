8b60f33d9dd4ae7134f3b76be021f4f1f9477ba2
Huang Xing <huangxing567@163.com>
2021-12-05 23:35:18 +0800

    Continue learn python list
    Signed-off-by: Huang Xing <huangxing567@163.com>

--- python/test/test_list2.py
+++ python/test/test_list2.py
@@ -7,16 +7,23 @@ import sys
 
 # 单链表类
 class LNode():
-    def __init__(self, elem):
+    def __init__(self, elem=0):
         self.elem = elem
         self.next = None
 
     def print(self):
-        print("list: ", sep='', end='')
+        print("list[", self.length(), "]: ", sep='', end='')
         while self is not None:
             print(self.elem, " ", sep='', end='')
             self = self.next
         print('')
+
+    def length(self):
+        len = 0
+        while self is not None:
+            len = len + 1
+            self = self.next
+        return len
 
     def insert_tail(self, elem):
         new = LNode(elem)
@@ -30,6 +37,15 @@ class LNode():
         self = new
         return self
 
+    def insert_index(self, index, elem):
+        new = LNode(elem)
+        i = 0
+        while self.next is not None and i < index :
+            self = self.next
+            i = i + 1
+        new.next = self.next
+        self.next = new
+
 def main():
     l = LNode(0)
     l.print()
@@ -38,6 +54,10 @@ def main():
     l.print()
     for num in range(6, 10):
         l = l.insert_head(num)
+    l.print()
+    l.insert_index(0, 10)
+    l.insert_index(2, 11)
+    l.insert_index(100, 12)
     l.print()
 
 if __name__ == '__main__':
