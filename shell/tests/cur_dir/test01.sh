349ccf1c6224d64ca99590752834bc6d5cdb1d21
Huang Xing <huangxing567@163.com>
2022-07-14 15:35:27 +0000

    Add shell test of get cur dir
    Signed-off-by: Huang Xing <huangxing567@163.com>

---
+++ shell/tests/cur_dir/test01.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cur_dir=$(cd $(dirname $BASH_SOURCE[0]); pwd)
+echo "cur dir: [$cur_dir]"
---
+++ shell/tests/cur_dir/test02/test02.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cur_dir=$(cd $(dirname $BASH_SOURCE[0]); pwd)
+echo "cur dir: [$cur_dir]"
+
+source $cur_dir/../test01.sh
