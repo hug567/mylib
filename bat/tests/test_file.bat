3d5f0ec12c8696f67913f355d795643e3a880d92
Huang Xing <huangxing567@163.com>
2022-07-31 23:23:07 +0800

    Learning windows bat script
    Signed-off-by: Huang Xing <huangxing567@163.com>

---
+++ bat/tests/test_file.bat
@@ -0,0 +1,34 @@
+::bat函数
+
+::不回显命令
+@echo off
+
+call :log_info "print info level log"
+
+::调用函数
+call :checkFileExist test_file.bat
+call :checkFileExist file.txt
+echo ------------------------
+
+::暂停
+pause
+::退出当前脚本
+exit
+
+::---------------------------------------------------------------------------::
+::检查文件是否存在
+:checkFileExist
+    set file=%1
+    if not exist %file% (
+        echo file [%file%] does not exist
+        pause
+        exit
+    )
+goto:eof
+
+:log_info
+    for /f "delims=" %%i in ('date') do set time=%%i
+    echo %time%
+    echo [('date')^][INFO ] %~1
+goto:eof
+::---------------------------------------------------------------------------::
