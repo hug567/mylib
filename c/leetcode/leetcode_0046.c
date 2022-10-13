a694cbd4d4c1bf8f8937ea648bf1da202c40514a
Huang Xing <huangxing567@163.com>
2022-10-13 21:45:17 +0800

    Add leetcode 46 with c
    Signed-off-by: Huang Xing <huangxing567@163.com>

---
+++ c/leetcode/leetcode_0046.c
@@ -0,0 +1,24 @@
+/*
+ * leetcode 46：全排列 [中等]
+ * 思路：
+ * 2022-09-25
+ */
+#include "common.h"
+
+/***** 提交代码 ******************************************************************/
+
+int** permute(int* nums, int numsSize, int* returnSize, int** returnColumnSizes)
+{
+
+}
+
+/***** 提交代码 ******************************************************************/
+
+void TestCase1(void)
+{}
+
+int main(void)
+{
+    TestCase1();
+    return 0;
+}
