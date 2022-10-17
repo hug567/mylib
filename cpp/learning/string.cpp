ab4b0abcfcb8ee20412f49db334122c47ec9c435
Huang Xing <huangxing567@163.com>
2022-10-17 14:58:21 +0000

    Continue learning cpp string
    Signed-off-by: Huang Xing <huangxing567@163.com>

--- cpp/learning/string.cpp
+++ cpp/learning/string.cpp
@@ -3,10 +3,22 @@
 #include <iostream>
 
 using std::cout;
+using std::wcout;
 using std::endl;
 using std::string;
 
-void string_usage(void)
+//c类型的字符串
+void string_like_c(void)
+{
+    const char *str1 = "c string 1";
+    cout << "str1: " << str1 << endl;
+
+    char str2[] = "c string 2";
+    cout << "str2: " << str2 << endl;
+}
+
+//string类
+void string_class(void)
 {
     // string定义
     string str1 = "test string 1";
@@ -34,12 +46,30 @@ void string_usage(void)
     cout << "str6: " << str6 << endl;
 
     // string长度
-    cout << "str6 len: " << str6.size() << endl;
+    cout << "str6 size: " << str6.size() << endl;
     cout << "str6 len: " << str6.length() << endl;
+}
+
+//其他类似的字符串
+static void string_others(void)
+{
+    //2字节宽度的字符
+    wchar_t str1[] = L"w_char string";
+    wcout << "str1: " << str1 << endl;
+
+    //2字节宽度的字符
+    char16_t str2[] = u"char16_t string";
+    //4字节宽度的字符
+    char32_t str3[] = U"char32_t string";
+
+    //原始(raw)字符串
+    cout << R"(This is a "raw" string)" << endl;
 }
 
 int main(void)
 {
-    string_usage();
+    string_like_c();
+    string_class();
+    string_others();
     return 0;
 }
