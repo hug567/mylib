aaaaab7b65e60b38de9abc099d2a7eb4fe651890
Huang Xing <huangxing567@163.com>
2022-12-15 22:30:31 +0800

    Add shell test_cmd.sh
    Signed-off-by: Huang Xing <huangxing567@163.com>

--- shell/tests/test_cmd.sh
+++ shell/tests/test_cmd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 逐行遍历命令的执行结果
-function traverse_cmd_resoult() {
+function traverse_cmd_result() {
     result=$(ls -l)
     echo "$result" # 加双引号会保留换行符
     # 逐行遍历结果
@@ -11,8 +11,14 @@ function traverse_cmd_resoult() {
     done
 }
 
+# 判断命令返回值
+function cmd_ret() {
+    echo "get cmd return value"
+    ls || exit # 命令执行失败将退出当前脚本
+}
+
 function main() {
-    traverse_cmd_resoult
+    traverse_cmd_result
 }
 
 main
