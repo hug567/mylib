9267e29cb610f5eae286cfebd902c5051daff8e3
Huang Xing <huangxing567@163.com>
2023-03-02 18:44:35 +0800

    Update vim.md

--- shell/tests/test_cmd.sh
+++ shell/tests/test_cmd.sh
@@ -2,6 +2,7 @@
 
 # 逐行遍历命令的执行结果
 function traverse_cmd_result() {
+    echo -e "\n[function]: $FUNCNAME"
     local result=$(ls -l)
     echo "$result" # 加双引号会保留换行符
     # 逐行遍历结果
@@ -13,12 +14,22 @@ function traverse_cmd_result() {
 
 # 判断命令返回值
 function cmd_ret() {
+    echo -e "\n[function]: $FUNCNAME"
     echo "get cmd return value"
     ls || exit # 命令执行失败将退出当前脚本
 }
 
+# echo输出换行
+function echo_long_line() {
+    echo -e "\n[function]: $FUNCNAME"
+    echo "line 1" \
+         "line 2" \
+         "line 3"
+}
+
 function main() {
     traverse_cmd_result
+    echo_long_line
 }
 
 main
