fcb66d9b3feb54a8159288c458f9b0e4599d69e0
Huang Xing <huangxing567@163.com>
2023-04-12 22:25:52 +0800

    Update yocto qemu_aarch64.sh

--- shell/yocto/qemu_aarch64.sh
+++ shell/yocto/qemu_aarch64.sh
@@ -5,6 +5,10 @@ CUR_SCRIPT_DIR=$(cd $(dirname $BASH_SOURCE[0]); pwd)
 export MYLIB=$(cd ${CUR_SCRIPT_DIR}/../../; pwd)
 source ${MYLIB}/shell/yocto/common.sh
 
+usage() {
+    log_info "$0 <minimal/full>  using qemu run kernel"
+}
+
 #qemu-system-aarch64 \
 #    -device virtio-net-device,netdev=net0,mac=52:54:00:12:34:02 \
 #    -netdev tap,id=net0,ifname=tap0,script=no,downscript=no \
@@ -21,7 +25,8 @@ source ${MYLIB}/shell/yocto/common.sh
 #    -kernel Image--5.15.32+git0+63e25b5717_387a676543-r0-qemuarm64-20230408154253.bin \
 #    -append 'root=/dev/vda rw mem=256M ip=192.168.7.2::192.168.7.1:255.255.255.0::eth0:off:8.8.8.8'
 
-main() {
+run_kernel() {
+    local image=$1
     check_files_exist ./tmp/deploy/images/qemuarm64/Image-qemuarm64.bin
 
     cd tmp/deploy/images/qemuarm64
@@ -29,7 +34,7 @@ main() {
         -M virt -cpu cortex-a57 -smp 4 -m 256 -nographic \
         -kernel Image-qemuarm64.bin \
         -device virtio-blk-device,drive=disk0 \
-        -drive id=disk0,file=core-image-minimal-qemuarm64.ext4,if=none,format=raw \
+        -drive id=disk0,file=${image},if=none,format=raw \
         -device virtio-net-device,netdev=net0,mac=52:54:00:12:34:02 \
         -netdev tap,id=net0,script=no,downscript=no,ifname=tap0 \
         -object rng-random,filename=/dev/urandom,id=rng0 \
@@ -41,4 +46,23 @@ main() {
         --append 'root=/dev/vda rw mem=256M ip=192.168.0.101::192.168.0.1:255.255.255.0::eth0:off:8.8.8.8'
 }
 
-main
+main() {
+    if [ "$#" != "1" ]; then
+        usage
+        exit 1
+    fi
+    local image=$1
+    case $image in
+        minimal)
+            run_kernel core-image-minimal-qemuarm64.ext4
+            ;;
+        full)
+            run_kernel core-image-full-cmdline-qemuarm64.ext4
+            ;;
+        *)
+            usage
+            exit 2
+    esac
+}
+
+main $*
