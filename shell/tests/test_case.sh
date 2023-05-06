627865be2b407cae7ecaf485f10a234f77c3855d
Huang Xing <huangxing567@163.com>
2023-05-06 23:28:51 +0800

    Update shell test_case.sh

--- shell/tests/test_case.sh
+++ shell/tests/test_case.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
-function case_use() {
-    local val=$h # 局部变量
+function _test_case() {
+    local var=$1
+    case $var in
+        1)
+            echo "var is 1"
+            ;;
+        2)
+            echo "var is 2"
+            ;;
+        3|4)
+            echo "var is 3 or 4"
+            ;;
+        *)
+            echo "unknown var: $var"
+    esac
 }
 
-function test_case01() {
-    echo "test_case01:"
+function test_case() {
+    echo "$FUNCNAME:-----------------------------"
+    _test_case 1
+    _test_case 2
+    _test_case 3
+    _test_case 4
+    _test_case 5
 }
 
 function main() {
-    test_case01
+    test_case
 }
 
 main
--- shell/tests/test_getopt.sh
+++ shell/tests/test_getopt.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
+# getopt不能处理带空格的参数
 function _test_getopt() {
+    local name=
+    local age=
+    local short=
+
+    set -- $(getopt a:n:s "$@")
+    while [ -n "$1" ]; do
+        case $1 in
+            -n) name=$2; shift;;
+            -a) age=$2; shift;;
+            -s) short=1;;
+        esac
+        shift
+    done
+    if [[ $short = 1 ]]; then
+        echo "Student: $name/$age"
+    else
+        echo "Student name: $name, age: $age"
+    fi
 }
 
 function test_getopt() {
     echo "$FUNCNAME:-----------------------------"
+    _test_getopt -n Bob -a 18 -s
+    _test_getopt -n Tom -a 19
 }
 
 function main() {
+    test_getopt
 }
 
 main
