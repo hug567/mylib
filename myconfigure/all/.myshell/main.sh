efd003943d6bc7111c2343e96950ca616b383793
Huang Xing <huangxing567@163.com>
2023-06-19 23:00:44 +0800

    Add .cur.shell support in unify configs

--- myconfigure/all/.myshell/main.sh
+++ myconfigure/all/.myshell/main.sh
@@ -6,6 +6,7 @@
 [ -f ~/.myshell/env.sh   ] && source ~/.myshell/env.sh
 [ -f ~/.myshell/path.sh  ] && source ~/.myshell/path.sh
 [ -f ~/.myshell/cmd.sh   ] && source ~/.myshell/cmd.sh
+[ -f ~/.cur.shell        ] && source ~/.cur.shell
 
 if is_bash; then
     [ -f ~/.mybash/main.sh ] && source ~/.mybash/main.sh
