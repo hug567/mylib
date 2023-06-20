43a7e16c1621291ec488c22dc92a82a2ee35dc0a
Huang Xing <huangxing567@163.com>
2023-06-20 16:28:47 +0800

    Learning replace str by sed in shell script

--- shell/tests/test_sed.sh
+++ shell/tests/test_sed.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-line=$(sed -n '/xxx_test.so/=' test.txt)
-line=(${line// / })
-line=${line[1]}
-sed -i "${line}"'s/xxx_test.so \\/xxx_test.so/g' test.txt
+function test_sed_replace() {
+    echo "$FUNCNAME:-----------------------------"
+
+    local str01="1234567890"
+    local str02=$(echo $str01 | sed 's/456/xxx/g')
+    echo "str01: $str01"
+    echo "str02: $str02"
+
+    local str01="1234567890xx%20yy"
+    local str02=$(echo $str01 | sed 's/%20/_/g')
+    echo "str01: $str01"
+    echo "str02: $str02"
+}
+
+function main() {
+    test_sed_replace
+}
+
+main
