db128bada6057d3da911dad84cc3809b7b307842
Huang Xing <huangxing567@163.com>
2023-06-26 14:59:38 +0800

    Update shell test_if.sh

--- shell/tests/test_if.sh
+++ shell/tests/test_if.sh
@@ -21,10 +21,12 @@ function test_func_ret_or_cmp() {
     echo "$FUNCNAME:-----------------------------"
 
     local name="Tom"
+    # multiple conditions of if
     if __return_false || [ $name == "Tom" ]; then
         echo "my name is Tom"
     fi
 
+    # Use not in multiple conditions
     if ! __return_false || [ "$name" == "Bob" ]; then
         echo "not return false or my name is Bob"
     fi
