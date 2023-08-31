45a77ecd2105a2a636761e1e5e35d28408c1672f
Huang Xing <huangxing567@163.com>
2023-08-31 18:03:52 +0800

    Add usually using api header

---
+++ c/tests/head.h
@@ -0,0 +1,19 @@
+// 常用接口/结构所在头文件
+
+#include <stdio.h>
+    printf
+
+#include <stdlib.h>
+    atoi
+
+#include <string.h>
+    memset
+
+#include <unistd.h>
+    close
+
+#include <sys/socket.h>
+    socket
+
+#include <sys/socket.h>
+    struct sockaddr_in
