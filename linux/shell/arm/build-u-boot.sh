55ffa9efd0f5e778182f669fe865810f717dc2f3
Huang Xing <huangxing567@163.com>
2023-09-11 23:12:31 +0800

    Update linux build script

--- linux/shell/arm/build-u-boot.sh
+++ linux/shell/arm/build-u-boot.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
-BLACK="\033[30m"
-RED="\033[31m"
-GREEN="\033[32m"
-YELLOW="\033[33m"
-BLUE="\033[34m"
-PURPLE="\033[35m"
-SKYBLUE="\033[36m"
-WHITE="\033[37m"
-RESET="\033[0m"
 
-#
 make clean
-#
 make mrproper
-# 生成配置文件
-make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- vexpress_ca9x4_config
-# 编译文件
-make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi-
+
+make -C ../ O=`pwd` ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- vexpress_ca9x4_config
+make -C ../ O=`pwd` ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- -j
+
 # 拷贝u-boot文件至tftp根目录
 sudo cp u-boot /var/lib/tftproot
+
 # qemu启动命令
-echo -e "\n${YELLOW}Verify: ${GREEN}qemu-system-arm -M vexpress-a9 -m 256M -kernel ./u-boot -nographic${RESET}"
+echo "Verify: qemu-system-arm -M vexpress-a9 -m 256M -kernel ./u-boot -nographic"
