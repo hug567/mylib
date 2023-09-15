94e25b065b8467a9a72d6c3619580cc76ae4f258
Huang Xing <huangxing567@163.com>
2023-09-15 23:06:25 +0800

    Update build-u-boot.sh

--- linux/shell/arm/build-u-boot.sh
+++ linux/shell/arm/build-u-boot.sh
@@ -3,11 +3,10 @@
 make clean
 make mrproper
 
+mkdir build-arm
+cd build-arm
 make -C ../ O=`pwd` ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- vexpress_ca9x4_config
 make -C ../ O=`pwd` ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- -j3
-
-# 拷贝u-boot文件至tftp根目录
-sudo cp u-boot /var/lib/tftproot
 
 # qemu启动命令
 echo "Verify: qemu-system-arm -M vexpress-a9 -m 256M -kernel ./u-boot -nographic"
