9b341d86b0d94aa448c889b06bf65ce1e3b10071
Huang Xing <huangxing567@163.com>
2023-11-08 23:06:33 +0800

    Update flask run script

--- web/flask/flask_0.12.2/run.sh
+++ web/flask/flask_0.12.2/run.sh
@@ -3,4 +3,27 @@
 export FLASK_DEBUG=1
 export FLASK_APP=main.py
 
-flask run -h 10.110.0.3 -p 9200
+function ip_exist() {
+    local ip=$1
+    local ret=$(ifconfig | grep "$ip")
+
+    if [ "$ret" == "" ]; then
+        return 1
+    fi
+    return 0
+}
+
+function main() {
+    local ip=""
+    local ip_addr=(
+        "10.110.0.3"
+        "172.20.115.98"
+    )
+    for ip in ${ip_addr[@]}; do
+        if ip_exist $ip; then
+            flask run -h $ip -p 9200
+        fi
+    done
+}
+
+main $*
