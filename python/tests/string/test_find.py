23abdaca7be2f38a1c69b947777fc0dddeb76776
Huang Xing <huangxing567@163.com>
2024-02-20 17:54:31 +0800

    Continue learning python string find

--- python/tests/string/test_find.py
+++ python/tests/string/test_find.py
@@ -2,9 +2,10 @@
 # 练习: 字符串查找
 # 时间：2023-10-11
 
+import re
 import sys
 
-#字符串搜索替换
+#字符串搜索
 def test_find():
     print(sys._getframe().f_code.co_name, ": -------------------------", sep='')
 
@@ -12,8 +13,25 @@ def test_find():
     if str1.find("Tom") >= 0:
         print("find \'Tom\' in str \'" + str1 + "\'")
 
+    # 查找的子串中包含空格
+    if str1.find(" is") >= 0:
+        print("find \'name is\' in str \'" + str1 + "\'")
+
+#使用正则表达式搜索
+def test_search():
+    print(sys._getframe().f_code.co_name, ": -------------------------", sep='')
+
+    # 正则表达式从头部开始查找
+    str1 = "My name is Tom!"
+    if re.search('^My', str1) is not None:
+        print("find \'My\' in str head: \'" + str1 + "\'")
+    str2 = "str: My name is Tom!"
+    if re.search('^My', str2) is None:
+        print("does not find \'My\' in str head: \'" + str2 + "\'")
+
 def main():
     test_find()
+    test_search()
 
 if __name__ == '__main__':
     main()
---
+++ python/tests/string/test_num.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+# 练习: 字符串与数字互相转换
+# 时间：2023-10-10
+
+import sys
+
+def test_str_to_int():
+    print(sys._getframe().f_code.co_name, ": -------------------------", sep='')
+
+    str1 = '1234'
+    num1 = int(str1)
+    print("num1 type: ", type(num1), ", num1 = ", num1, sep='')
+
+    str2 = '005678'
+    num2 = int(str2)
+    print("num2 type: ", type(num2), ", num2 = ", num2, sep='')
+
+def test_str_to_float():
+    print(sys._getframe().f_code.co_name, ": -------------------------", sep='')
+
+    str1 = '1234.56'
+    num1 = float(str1)
+    print("num1 type: ", type(num1), ", num1 = ", num1, sep='')
+
+    str2 = '78.900'
+    num2 = float(str2)
+    print("num2 type: ", type(num2), ", num2 = ", num2, sep='')
+
+def test_float_to_string():
+    print(sys._getframe().f_code.co_name, ": -------------------------", sep='')
+
+    num1 = 1234.56
+    str1 = str(num1)
+    print("str1 type: ", type(str1), ", str1 = ", str1, sep='')
+
+    num2 = 78.900
+    str2 = str(num2)
+    print("str2 type: ", type(str2), ", str2 = ", str2, sep='')
+
+    # 浮点型整数转字符串，会在小数点后保留1个0
+    num1 = 234.00009
+    str1 = str(num1)
+    print("str1 type: ", type(str1), ", str1 = ", str1, sep='')
+    # 去除末尾的.0
+    str1 = str(num1).rstrip('0').rstrip('.')
+    print("str1 type: ", type(str1), ", str1 = ", str1, sep='')
+
+def main():
+    test_str_to_int()
+    test_str_to_float()
+    test_float_to_string()
+
+if __name__ == '__main__':
+    main()
