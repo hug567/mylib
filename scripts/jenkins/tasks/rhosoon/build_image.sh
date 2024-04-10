2a68c39834813ad6f8b88b32de23f580c7d3482a
Huang Xing <huangxing567@163.com>
2024-04-10 16:02:16 +0800

    Add jenkins files

---
+++ scripts/jenkins/tasks/rhosoon/build_image.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+# 2024-04-10
+
+WORK_DIR="/mnt/sdb/jenkins/source_code/test01"
+CUR_DIR=$(cd $(dirname $BASH_SOURCE[0]); pwd)
+LOGFMT="build_image"
+
+function log_with_level() {
+    local level=$1
+    local logs="${@:2}"
+    echo -e "[$(date '+%Y-%m-%d %H:%M:%S')][$LOGFMT][$level] $logs"
+}
+
+function log_info() {
+    log_with_level "INFO" "$*"
+}
+
+function log_warn() {
+    log_with_level "WARN" "$*"
+}
+
+function log_err() {
+    log_with_level "ERROR" "$*"
+}
+
+function repo_download_code() {
+    local work_dir=$1
+
+    if [ ! -d $work_dir ]; then
+        mkdir -p $work_dir
+    fi
+    cd $work_dir
+    rm -rf $(ls -1A)
+    repo init -u http://192.168.99.221:3000/Rhosoon_RD/manifest.git --repo-url=http://192.168.99.221:3000/Rhosoon_RD/git-repo.git --repo-rev=main --no-clone-bundle
+    repo sync
+    repo forall -c 'git lfs pull'
+    repo start main --all
+}
+
+function delete_old_docker_container() {
+    local ids=
+
+    ids=$(docker ps -a | grep rhosoon_docker_ | awk '{print$1}')
+    if [ "$ids" = "" ]; then
+        return
+    fi
+    echo "$ids" | while read id; do
+        log_info "will stop and rm container: $id"
+        docker stop $id
+        docker rm $id
+    done
+}
+
+function launch_docker() {
+    local image="rhosoon_ubuntu_18:v0.5"
+    local name="rhosoon_docker_$(date '+%Y%m%d%H%M%S')"
+
+    delete_old_docker_container
+
+    log_info "will launch docker with name: $name"
+    PS1="[host] $ "
+    echo $PS1
+expect <<EOF
+    set timeout 43200
+    spawn docker run -it \
+        --user rhosoon -w /home/rhosoon \
+        -v /mnt/sdb:/mnt/sdb \
+        -v /opt/pkg:/opt/pkg \
+        -v /tmp/docker:/tmp/docker \
+        -v ${HOME}/repo:/home/rhosoon/repo \
+        -v ${HOME}/code:/home/rhosoon/code \
+        -v ${HOME}/temp:/home/rhosoon/temp \
+        -v ${HOME}/tools:/home/rhosoon/tools \
+        -v ${HOME}/share:/home/rhosoon/share \
+        --net=host \
+        --name=${name} \
+        ${image} \
+        /bin/bash
+    sleep 5
+    send "\rPS1='\[\\\u@\\\h\] \\\w $ '\r"
+    expect {
+        "\[rhosoon@" { send "/tmp/docker/build_image_in_docker.sh\r"}
+        timeout { puts "docker build timeout"; exit 127 }
+    }
+    expect {
+        "finish building all images in docker" {}
+        timeout { puts "docker build timeout"; exit 127 }
+    }
+EOF
+
+    delete_old_docker_container
+}
+
+function main() {
+    repo_download_code $WORK_DIR
+    cp $CUR_DIR/build_image_in_docker.sh /tmp/docker
+    launch_docker
+    #build_all_platform
+}
+
+main $*
---
+++ scripts/jenkins/tasks/rhosoon/build_image_in_docker.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+# 2024-04-10
+
+WORK_DIR="/mnt/sdb/jenkins/source_code/test01"
+IMAGES_DIR="/mnt/sdb/jenkins/images"
+LOGFMT="build_image_in_docker"
+
+function log_with_level() {
+    local level=$1
+    local logs="${@:2}"
+    echo -e "[$(date '+%Y-%m-%d %H:%M:%S')][$LOGFMT][$level] $logs"
+}
+
+function log_info() {
+    log_with_level "INFO" "$*"
+}
+
+function log_warn() {
+    log_with_level "WARN" "$*"
+}
+
+function log_err() {
+    log_with_level "ERROR" "$*"
+}
+
+function build_petalinux() {
+    local project=$1
+    local platform=$2
+    local work_dir=$WORK_DIR
+
+    log_info "will build $platform $project -----------------------------------"
+    cd $work_dir/$project
+    rm -rf build
+    git clean -fdx
+    if [ "$platform" = "default" ]; then
+        ../build.sh
+    else
+        ../build.sh $platform
+    fi
+    return $?
+}
+
+function build_yocto() {
+    local platform=$1
+    local plat_image_dir=$2
+    local image_dir=$WORK_DIR/yocto/images/linux
+
+    if ! build_petalinux yocto $platform; then
+        log_err "build $platform yocto failed"
+        exit 1
+    fi
+    cp $image_dir/BOOT.BIN $plat_image_dir
+    cp $image_dir/image.ub $plat_image_dir
+    cp $image_dir/rootfs.tar.bz2 $plat_image_dir
+}
+
+function build_mkpart() {
+    local platform=$1
+    local plat_image_dir=$2
+    local image_dir=$WORK_DIR/ramfs/yocto-mkpart/images/linux
+
+    if ! build_petalinux ramfs/yocto-mkpart $platform; then
+        log_err "build $platform ramfs/yocto-mkpart failed"
+        exit 1
+    fi
+    cp $image_dir/BOOT-MKPART.BIN $plat_image_dir
+    cp $image_dir/image-mkpart.ub $plat_image_dir
+}
+
+function build_ramfs() {
+    local platform=$1
+    local plat_image_dir=$2
+    local image_dir=$WORK_DIR/ramfs/yocto-ramfs/images/linux
+
+    build_petalinux ramfs/yocto-ramfs $platform
+    if ! build_petalinux ramfs/yocto-ramfs $platform; then
+        log_err "build $platform ramfs/yocto-ramfs failed"
+        exit 1
+    fi
+    cp $image_dir/image-ramfs.ub $plat_image_dir
+}
+
+function build_one_platform() {
+    local platform=$1
+    local images_dir=$2
+    local plat_image_dir=$images_dir/$platform
+
+    log_info "will build platform: $platform ----------------------------------"
+    if [ ! -d $plat_image_dir ]; then
+        mkdir -p $plat_image_dir
+    fi
+    build_yocto $platform $plat_image_dir
+    build_mkpart $platform $plat_image_dir
+    build_ramfs $platform $plat_image_dir
+}
+
+# default, QQF, s300, ...
+function build_all_platform() {
+    local work_dir=$WORK_DIR
+    local images_dir=$IMAGES_DIR/$(date '+%Y%m%d_%H%M%S')
+
+    log_info "will build all platform images"
+    cd $work_dir
+    log_info "pwd: $(pwd)"
+    log_info "os-release: -----------------------------------------------------"
+    cat /etc/os-release
+    ls -la /
+
+    build_one_platform "default" $images_dir
+    build_one_platform "QQF" $images_dir
+    build_one_platform "s300" $images_dir
+}
+
+function main() {
+    build_all_platform
+    log_info "finish building all images in docker"
+}
+
+main $*
