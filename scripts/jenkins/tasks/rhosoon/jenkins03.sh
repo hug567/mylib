37c1e0c6d1093607e0364ff39e91924e7b67801c
Huang Xing <huangxing567@163.com>
2024-04-12 13:53:57 +0800

    Add startup jenkins scripts

---
+++ scripts/jenkins/tasks/rhosoon/jenkins03.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# 2024-04-10
+
+function run_jenkins_docker() {
+    docker run \
+        -u root --rm -d \
+        -p 8080:8080 \
+        -p 50000:50000 \
+        -v /disk/sda:/disk/sda \
+        -v /opt/pkg:/opt/pkg \
+        -v /tmp/docker:/tmp/docker \
+        -v $HOME/repo:/root/repo \
+        -v $HOME/code:/root/code \
+        -v $HOME/temp:/root/temp \
+        -v $HOME/tools:/root/tools \
+        -v $HOME/share:/root/share \
+        -v $HOME/code/jenkins/home:/var/jenkins_home \
+        -v $HOME/code/jenkins/root:/root \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        myjenkins:v0.2
+        #jenkins/jenkins:lts-jdk11
+}
+
+function main() {
+    run_jenkins_docker
+}
+
+main $*
