2bb8eeb974b349185ba5ec24ddcdc3e29971b2a4
Huang Xing <huangxing567@163.com>
2024-04-15 17:59:08 +0800

    Update iptables_net_forward.sh

--- shell/tools/iptables_net_forward.sh
+++ shell/tools/iptables_net_forward.sh
@@ -5,18 +5,30 @@ export MYLIB=$(cd ${SCRIPT_DIR}/../../; pwd)
 source ${MYLIB}/shell/lib/common.sh
 
 function usage() {
-    log_info "$0 <ip> enable      enable iptables forward ip"
-    log_info "$0 <ip> disalbe     disable iptables forward ip"
+    log_info "$0 enable <inside_ip> <outside_ip>      enable iptables forward inside ip"
+    log_info "$0 disalbe <inside_ip>                  disable iptables forward inside ip"
 }
 
+function delete_ip_forward() {
+    local ip=$1
+}
+
+function add_ip_forward() {
+    local ip=$1
+}
 
 function main() {
-    if [ $# -ne 2 ]; then
+    if [ "$1" = "-h" ]; then
+        usage
+        exit 0
+    fi
+    if [ $# -lt 2 -o $# -gt 3 ]; then
         usage
         exit 1
     fi
-    local ip=$1
-    local set=$2 # enable / disable
+    local set=$1 # enable / disable
+    local inside_ip=$2
+    local outside_ip=$3
 }
 
 main $*
