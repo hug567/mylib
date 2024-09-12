a3f0ded5af559a3dd7566110e068343ae079986e
Huang Xing <huangxing567@163.com>
2024-09-12 23:07:26 +0800

    Update dockerfile of ubuntu 18

--- docker/dockerfile/ubuntu/18/build.sh
+++ docker/dockerfile/ubuntu/18/build.sh
@@ -1,49 +1,28 @@
 #!/bin/bash
 
-MYLIB="$HOME/code/mylib"
-
-function prepare_files() {
-    local tmpdir=$1
-
-    cp $MYLIB/docker/dockerfile/ubuntu/18/base.df $tmpdir
-    cp $MYLIB/myconfigure/ubuntu/18.04/sources.list $tmpdir
-
-    mkdir -p $tmpdir/homefiles
-    local config_files=(
-        .gitconfig
-        .minttyrc
-        .myshell
-        .oh-my-zsh
-        .profile
-        .tigrc
-        .tmux
-        .tmux.conf
-        .vim
-        .vimrc
-        .zshrc
-    )
-    for file in ${config_files[@]}; do
-        cp -r $MYLIB/myconfigure/all/$file $tmpdir/homefiles
-    done
-}
-
-function build_image() {
-    local tmpdir=$1
-
-    cd $tmpdir
-    docker build -t myubuntu18:v0.1 -f base.df ./
+function usage() {
+    echo "Usage: $0 <name>"
+    echo "   eg: $0 myubuntu18"
+    echo "   eg: $0 myubuntu18.df"
 }
 
 function main() {
-    local tmpdir="/tmp/dockerfile/ubuntu/18"
+    local name=$1
+    local version="v0.1"
 
-    if [ -d $tmpdir ]; then
-        rm -rf $tmpdir
+    if [ $# -ne 1 ]; then
+        usage
+        exit
     fi
-    mkdir -p $tmpdir
-
-    prepare_files $tmpdir
-    build_image $tmpdir
+    if [[  "${name}" =~ .*\.df$ ]]; then
+        name=$(echo "${name}" | sed 's/\(.*\)\.df$/\1/g')
+    fi
+    if [ ! -f ${name}.df ]; then
+        echo "there is no file: ${name}.df"
+        exit
+    fi
+    echo "will build ${name}:${version} by ${name}.df"
+    docker build -t ${name}:${version} -f ${name}.df ./
 }
 
 main $*
--- docker/dockerfile/ubuntu/18/run.sh
+++ docker/dockerfile/ubuntu/18/run.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
 
-docker run \
-    --net=host \
-    -v /tmp/docker:/tmp/docker \
-    -v /home/hx/code:/home/hx/code \
-    -v /home/hx/share:/home/hx/share \
-    -v /home/hx/temp:/home/hx/temp \
-    -v /home/hx/tools:/home/hx/tools \
-    -v /home/hx/workspace:/home/hx/workspace \
-    -v /run/dbus/system_bus_socket:/run/dbus/system_bus_socket:ro \
-    --hostname DockerUbuntu18 \
-    -it myubuntu18:v0.1 \
-    /bin/bash
+function run_in_work_compute() {
+    docker run \
+        --net=host \
+        -v /tmp/docker:/tmp/docker \
+        -v /home/hx/code:/home/hx/code \
+        -v /home/hx/share:/home/hx/share \
+        -v /home/hx/temp:/home/hx/temp \
+        -v /home/hx/tools:/home/hx/tools \
+        -v /home/hx/workspace:/home/hx/workspace \
+        -v /run/dbus/system_bus_socket:/run/dbus/system_bus_socket:ro \
+        --hostname DockerUbuntu18 \
+        -it myubuntu18:v0.1 \
+        /bin/bash
+}
+
+function run_in_personal_wsl2() {
+    docker run \
+        --net=host \
+        -v ${HOME}/code:/home/hx/code \
+        -v ${HOME}/temp:/home/hx/temp \
+        -v ${HOME}/tools:/home/hx/tools \
+        --hostname DockerUbuntu18 \
+        -it myubuntu18:v0.1 \
+        /bin/bash
+}
+
+function main() {
+    local machine=$1
+
+    if [ "${machine}" == "work" ]; then
+        run_in_work_compute
+    elif [ "${machine}" == "personal" ]; then
+        run_in_personal_wsl2
+    fi
+}
+
+main $*
