080ac592748589a50966bfd4406978aff5d30847
Huang Xing <huangxing567@163.com>
2024-11-07 19:28:25 +0800

    Continue learning sh script

--- shell/tests/sh/test_logic.sh
+++ shell/tests/sh/test_logic.sh
@@ -25,8 +25,29 @@ test_logic() {
     fi
 }
 
+_print_age_by_name() {
+    local name=${1}
+
+    if [ "${name}" = "Tom" ]; then
+        echo "Tom age is 18"
+    elif [ "${name}" = "Bob" ]; then
+        echo "Bob age is 19"
+    else
+        echo "${name} age is 20"
+    fi
+}
+
+test_if_elif() {
+    echo "[test_if_elif]:------------------------------------------------------"
+
+    _print_age_by_name "Tom"
+    _print_age_by_name "Bob"
+    _print_age_by_name "Jerry"
+}
+
 main() {
     test_logic
+    test_if_elif
 }
 
 main
