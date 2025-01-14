dac1e504dbe09f26f6c40c254418b7bfd5a07430
Huang Xing <huangxing567@163.com>
2025-01-14 09:57:52 +0800

    Add script for building all platforms cpu1_tools

---
+++ shell/tools/zynq/build_cpu1_toos_all.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+function build_all_platforms() {
+    local all_platforms=(
+        "QQE"
+        "QQF"
+        "QQFC"
+        "QQFT"
+        "QQFTC"
+        "S100AT"
+        "S300BT"
+        "S300F"
+        "S400AT"
+        "S450AT"
+        "S450AY"
+        "UWB"
+    )
+    local platform
+
+    for platform in ${all_platforms[@]}; do
+        echo "[INFO][${platform}]: --------------------------------------------"
+        if [ -d build ]; then
+            rm -rf build
+        fi
+        ./scripts/build.sh ${platform} &> "/tmp/cpu1_tools_${platform}.log"
+        if [ $? -ne 0 ]; then
+            echo "[ERROR] build platform ${platform} failed"
+            exit 1
+        fi
+        echo "[INFO] build platform ${platform} success"
+    done
+    echo "[INFO] build all platforms success: ${all_platforms[@]}"
+}
+
+function main() {
+    local cur_dir=$PWD
+    local flag_file="include/share.h"
+
+    if [ ! -f ${flag_file} ]; then
+        echo "[ERROR] current is not in cpu1_tools repo"
+        exit 1
+    fi
+
+    build_all_platforms
+}
+
+main $*
