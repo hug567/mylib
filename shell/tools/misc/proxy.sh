071b48cde78d4a64ccb81457fe2c6a2f74bfe4cb
Huang Xing <huangxing567@163.com>
2025-04-28 10:32:01 +0800

    Add shell misc script proxy.sh

---
+++ shell/tools/misc/proxy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#export http_proxy="http://127.0.0.1:7890"; export https_proxy="http://127.0.0.1:7890"
+
+export http_proxy="http://10.110.0.4:10811"; export https_proxy="http://10.110.0.4:10811"
+
+$*
